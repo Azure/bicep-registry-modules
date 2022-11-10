8c815eb81ef9838db5c6528bc32aaa87d50ae9a2
Bhavya Subramanian <bhsubra@microsoft.com>
2022-11-10 09:13:52 -0800

    Added logic to get module names and tags

---
+++ scripts/github-actions/getModuleNamesWithTags.js
@@ -0,0 +1,50 @@
+/**
+ * @param {typeof import("fs")} fs
+ * @param {string} dir
+ */
+ function getSubdirNames(fs, dir) {
+  return fs
+    .readdirSync(dir, { withFileTypes: true })
+    .filter((x) => x.isDirectory())
+    .map((x) => x.name);
+}
+
+async function getModuleNamesWithTags({ require, core }) {
+  const fs = require("fs");
+  const path = require("path");
+  const axios = require("axios").default;
+
+  const moduleGroups = getSubdirNames(fs, "modules");
+  const result = [];
+
+  for (const moduleGroup of moduleGroups) {
+    var moduleGroupPath = path.join("modules", moduleGroup);
+    var moduleNames = getSubdirNames(fs, moduleGroupPath);
+
+    for (const moduleName of moduleNames) {
+      const modulePath = `${moduleGroup}/${moduleName}`;
+      const versionListUrl = `https://mcr.microsoft.com/v2/bicep/${modulePath}/tags/list`;
+
+      try {
+        const versionListResponse = await axios.get(versionListUrl);
+        const tags = versionListResponse.data.tags.sort();
+        const moduleNameWithTag = new ModuleNameWithTags(moduleName, tags);
+        
+        result.push(moduleNameWithTag);
+      } catch (error) {
+        core.setFailed(error);
+      }
+    }
+
+    fs.writeFile("moduleNamesWithTags.json", result);
+  }
+}
+
+export class ModuleNameWithTags {
+  constructor(name, tags) {
+    this.name = name;
+    this.tags = tags;
+  }
+}
+
+module.exports = getModuleNamesWithTags;
