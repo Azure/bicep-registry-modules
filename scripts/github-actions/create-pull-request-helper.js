9aa073b58b819f71d8b535b45a48562fd849706d
Bhavya Subramanian <bhsubra@microsoft.com>
2022-12-14 23:41:57 -0800

    Remove type: module

--- scripts/github-actions/create-pull-request-helper.js
+++ scripts/github-actions/create-pull-request-helper.js
@@ -1,65 +1,65 @@
-function getTimestamp() {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = (now.getMonth() + 1).toString().padStart(2, "0");
-  const date = now.getDate().toString().padStart(2, "0");
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-
-  return `${year}${month}${date}${hours}${minutes}${seconds}`;
-}
-
 async function createPullRequest(base, branchPrefix, contents, context, github, message, path, title) {
-    const branch = `${this.branchPrefix}-${this.getTimestamp()}`;
+    const branch = `${branchPrefix}-${getTimestamp()}`;
 
     // Create a new branch.
-    await this.github.rest.git.createRef({
-      ...this.context.repo,
+    await github.rest.git.createRef({
+      ...context.repo,
       ref: `refs/heads/${branch}`,
-      sha: this.context.sha,
+      sha: context.sha,
     });
 
     // Update file.
-    const { data: treeData } = await this.github.rest.git.createTree({
-      ...this.context.repo,
+    const { data: treeData } = await github.rest.git.createTree({
+      ...context.repo,
       tree: [
         {
           type: "blob",
           mode: "100644",
-          path: this.path,
-          content: this.contents,
+          path: path,
+          content: contents,
         },
       ],
-      base_tree: this.context.sha,
+      base_tree: context.sha,
     });
 
     // Create a commit.
-    const { data: commitData } = await this.github.rest.git.createCommit({
-      ...this.context.repo,
-      message: this.message,
+    const { data: commitData } = await github.rest.git.createCommit({
+      ...context.repo,
+      message: message,
       tree: treeData.sha,
-      parents: [this.context.sha],
+      parents: [context.sha],
     });
 
     // Update HEAD of the new branch.
-    await this.github.rest.git.updateRef({
-      ...this.context.repo,
+    await github.rest.git.updateRef({
+      ...context.repo,
       // The ref parameter for updateRef is not the same as createRef.
       ref: `heads/${branch}`,
       sha: commitData.sha,
     });
 
     // Create a pull request.
-    const { data: prData } = await this.github.rest.pulls.create({
-      ...this.context.repo,
-      title: this.title,
+    const { data: prData } = await github.rest.pulls.create({
+      ...context.repo,
+      title: title,
       head: branch,
-      base: this.base,
+      base: base,
       maintainer_can_modify: true,
     });
 
     return prData.html_url;
   }
 
+  function getTimestamp() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, "0");
+    const date = now.getDate().toString().padStart(2, "0");
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+  
+    return `${year}${month}${date}${hours}${minutes}${seconds}`;
+  }
+  
   module.exports = createPullRequest;
