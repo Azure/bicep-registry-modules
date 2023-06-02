a7a4c7e6f9a9b048f1f4ad91df4bc42153b0d5a8
Stephen Weatherford (MSFT) <stephwe@microsoft.com>
2023-06-02 13:20:11 -0700

    prettier

--- scripts/github-actions/test/publish-module-index.js
+++ scripts/github-actions/test/publish-module-index.js
@@ -1,14 +1,17 @@
 // Run from repo root
 const path = require("path");
-const script = require(path.join(process.cwd(), "scripts/github-actions/generate-module-index-data.js"));
+const script = require(path.join(
+  process.cwd(),
+  "scripts/github-actions/generate-module-index-data.js"
+));
 
 let core = {
-    info: function(s) {
-        console.warn(s);
-    },
-    setFailed: function(s) {
-        throw s;
-    }
+  info: function (s) {
+    console.warn(s);
+  },
+  setFailed: function (s) {
+    throw s;
+  },
 };
 
-script({ require, core })
+script({ require, core });
