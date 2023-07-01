6ebb00eb017b79cab0be8cfcae5b6ea6ee4243fd
Stephen Weatherford <Stephen.Weatherford.com>
2023-07-01 14:42:06 -0700

    w

--- scripts/local/create-prs-for-changed-modules.js
+++ scripts/local/create-prs-for-changed-modules.js
@@ -2,20 +2,21 @@
 
   This script creates a PR for module on the local disk that has changes
 
-  Usage:
-    cd repos/bicep-registry-modules
-    export GITHUB_PAT=...
-    export GITHUB_OWNER=...   (e.g. "azure" for "azure/bicep-registry-modules")
-    node scripts/local/create-prs-for-changed-modules.js <branch-prefix>
-
 */
+
+const usage = `Usage:
+cd repos/bicep-registry-modules
+export GITHUB_PAT=...
+export GITHUB_OWNER=...   (e.g. "azure" for "azure/bicep-registry-modules")
+node scripts/local/create-prs-for-changed-modules.js <branch-prefix> <title>
+
+e.g.
+node scripts/local/create-prs-for-changed-modules.js myname "feat: My PR Title"
+`;
 
 const { exec } = require("child_process");
 const { promisify } = require("util");
 const execPromise = promisify(exec);
-const { argv } = require("process");
-
-const { branchPrefix } = argv.slice(2);
 
 const red = "\u001b[31m";
 const green = "\u001b[32m";
@@ -23,6 +24,18 @@ const blue = "\u001b[34m";
 const yellow = "\u001b[33m";
 const reset = "\u001b[0m";
 
+const { argv } = require("process");
+
+const [branchPrefix, title] = argv.slice(2);
+console.log(`${green}branchPrefix: ${branchPrefix}${reset}`);
+console.log(`${green}title: ${title}${reset}`);
+
+if (!branchPrefix || !title) {
+  console.error(usage);
+  process.exit(1);
+}
+
+let runAll = false;
 function queryUserAsync(question) {
   const readline = require("readline").createInterface({
     input: process.stdin,
@@ -37,7 +50,7 @@ function queryUserAsync(question) {
   });
 }
 
-async function run(cmd, echo = true) {
+async function runAsync(cmd, echo = true) {
   if (echo) {
     console.log(cmd);
   }
@@ -47,6 +60,18 @@ async function run(cmd, echo = true) {
     console.warn(`> ${response.stdout}`);
   }
   return response.stdout;
+}
+
+async function queryRunAsync(cmd) {
+  const answer =
+    runAll || (await queryUserAsync(`Run "${yellow}${cmd}${reset}"? (y/n/a) `));
+
+  if (answer === "y") {
+    return await runAsync(cmd);
+  } else if (answer === "a") {
+    runAll = true;
+    return await runAsync(cmd, false);
+  }
 }
 
 async function getChangedModulesAsync() {
@@ -60,33 +85,36 @@ async function getChangedModulesAsync() {
   return changedModules;
 }
 
-async function CreatePR(modulePath) {
+async function CreatePRAsync(modulePath) {
   console.log(`Creating PR for ${modulePath}...`);
 
   const branchName = `${branchPrefix}/${modulePath}`;
-  await run(`git checkout -b ${branchName}`);
-  await run(`git add modules/${modulePath}`);
-  await run(`git status`);
-  await run(`git checkout main`);
-  await run(`git branch -d ${branchName}`);
+  await runAsync(`git checkout -b ${branchName}`);
+  await runAsync(`git add modules/${modulePath}`);
+  await runAsync(`git status`);
+
+  await queryRunAsync(`git status`);
+
+  await runAsync(`git checkout main`);
+  await runAsync(`git branch -d ${branchName}`);
   console.log();
 }
 
 async function CreatePRs() {
   const changedModules = await getChangedModulesAsync();
 
-  const currentBranch = await run(`git symbolic-ref --short HEAD`, false);
+  const currentBranch = await runAsync(`git symbolic-ref --short HEAD`, false);
   console.log(`${green}Current branch: ${currentBranch}${reset}`);
 
   try {
-    await run(`git checkout main`);
+    await runAsync(`git checkout main`);
 
     for (const modulePath of changedModules) {
-      await CreatePR(modulePath);
+      await CreatePRAsync(modulePath);
       break;
     }
   } finally {
-    await run(`git checkout ${currentBranch}`);
+    await runAsync(`git checkout ${currentBranch}`);
     console.log(`${green}Current branch: ${currentBranch}${reset}`);
   }
 }
