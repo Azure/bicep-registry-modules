07be5d42525c12dfbb305c84317ba7e0a05b561f
AlexanderSehr <asehr@hotmail.de>
2023-09-19 17:16:25 +0100

    Update to latest

--- avm/utilities/pipelines/resourcePublish/Publish-ToPublicBicepRegistry.ps1
+++ avm/utilities/pipelines/resourcePublish/Publish-ToPublicBicepRegistry.ps1
@@ -1,61 +1,60 @@
 function Publish-ToPublicBicepRegistry {
 
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $TemplateFilePath,
 
-  [CmdletBinding()]
-  param (
-    [Parameter(Mandatory = $true)]
-    [string] $TemplateFilePath,
+        [Parameter(Mandatory = $true)]
+        [secureString] $PublicRegistryServer
+    )
 
-    [Parameter(Mandatory = $true)]
-    [secureString] $PublicRegistryServer
-  )
+    # Load used functions
+    . (Join-Path $PSScriptRoot 'helper' 'Test-ModuleQualifiesForPublish.ps1')
+    . (Join-Path $PSScriptRoot 'helper' 'Get-ModuleTargetVersion.ps1')
+    . (Join-Path $PSScriptRoot 'helper' 'Get-BRMRepositoryName.ps1')
+    . (Join-Path $PSScriptRoot 'helper' 'Get-ModuleReadmeLink.ps1')
+    . (Join-Path $PSScriptRoot 'helper' 'Publish-ModuleToPrivateBicepRegistry.ps1')
 
-  # Load used functions
-  . (Join-Path $PSScriptRoot 'helper' 'Test-ModuleQualifiesForPublish.ps1')
-  . (Join-Path $PSScriptRoot 'helper' 'Get-ModuleTargetVersion.ps1')
-  . (Join-Path $PSScriptRoot 'helper' 'Get-BRMRepositoryName.ps1')
-  . (Join-Path $PSScriptRoot 'helper' 'Get-ModuleReadmeLink.ps1')
-  . (Join-Path $PSScriptRoot 'helper' 'Publish-ModuleToPrivateBicepRegistry.ps1')
+    $moduleRelativePath = Split-Path $TemplateFilePath -Parent
+    $moduleJsonFilePath = Join-Path $moduleRelativePath 'main.json'
 
-  $moduleRelativePath = Split-Path $TemplateFilePath -Parent
-  $moduleJsonFilePath = Join-Path $moduleRelativePath 'main.json'
-
-  # 1. Test if module qualifies for publishing
-  if (-not (Test-ModuleQualifiesForPublish -ModuleRelativePath $moduleRelativePath)) {
-    Write-Verbose "No changes detected. Skipping publishing" -Verbose
-    return
-  }
-
-  # 2. Calculate the version that we would publish with
-  $targetVersion = Get-ModuleTargetVersion -ModuleRelativePath $moduleRelativePath
-
-  # 3. Get Target Published Module Name
-  $publishedModuleName = Get-BRMRepositoryName -TemplateFilePath $TemplateFilePath
-
-  # 4. Get the documentation link
-  $documentationUri = Get-ModuleReadmeLink -ModuleRelativePath $moduleRelativePath
-
-  # 5. Replace telemetry version value (in JSON)
-  $tokenConfiguration = @{
-    FilePathList = @($moduleJsonFilePath)
-    Tokens       = @{
-      'ModuleTelemetryVersion' = $targetVersion # TODO : Check if we need to replace '.' with '-'
+    # 1. Test if module qualifies for publishing
+    if (-not (Test-ModuleQualifiesForPublish -ModuleRelativePath $moduleRelativePath)) {
+        Write-Verbose "No changes detected. Skipping publishing" -Verbose
+        return
     }
-    TokenPrefix  = '[['
-    TokenSuffix  = ']]'
-  }
-  $null = Convert-TokensInFileList @tokenConfiguration
 
-  #################
-  ##   Publish   ##
-  #################
-  $plainPublicRegistryServer = ConvertFrom-SecureString $PublicRegistryServer -AsPlainText
+    # 2. Calculate the version that we would publish with
+    $targetVersion = Get-ModuleTargetVersion -ModuleRelativePath $moduleRelativePath
 
-  $publishInput = @(
-    $moduleJsonFilePath
-    '--target', ("br:{0}/public/bicep/{1}:{2}" -f $plainPublicRegistryServer, $publishedModuleName, $targetVersion)
-    '--documentationUri', $documentationUri
-    '--force'
-  )
-  # bicep publish @publishInput
+    # 3. Get Target Published Module Name
+    $publishedModuleName = Get-BRMRepositoryName -TemplateFilePath $TemplateFilePath
+
+    # 4. Get the documentation link
+    $documentationUri = Get-ModuleReadmeLink -ModuleRelativePath $moduleRelativePath
+
+    # 5. Replace telemetry version value (in JSON)
+    $tokenConfiguration = @{
+        FilePathList = @($moduleJsonFilePath)
+        Tokens       = @{
+            'moduleVersion' = $targetVersion
+        }
+        TokenPrefix  = '[['
+        TokenSuffix  = ']]'
+    }
+    $null = Convert-TokensInFileList @tokenConfiguration
+
+    #################
+    ##   Publish   ##
+    #################
+    $plainPublicRegistryServer = ConvertFrom-SecureString $PublicRegistryServer -AsPlainText
+
+    $publishInput = @(
+        $moduleJsonFilePath
+        '--target', ("br:{0}/public/bicep/{1}:{2}" -f $plainPublicRegistryServer, $publishedModuleName, $targetVersion)
+        '--documentationUri', $documentationUri
+        '--force'
+    )
+    # bicep publish @publishInput
 }
