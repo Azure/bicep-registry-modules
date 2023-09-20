4aee824280decf4ef53efe50bde0eb0726a1bda1
Erika Gressi <erikag@microsoft.com>
2023-09-20 16:25:19 +0200

    draft module version

--- avm/utilities/pipelines/resourcePublish/helper/Get-ModuleTargetVersion.ps1
+++ avm/utilities/pipelines/resourcePublish/helper/Get-ModuleTargetVersion.ps1
@@ -19,17 +19,34 @@ Mandatory. The relative path of the module e.g. `avm/res/network/private-endpoin
 
 function Get-ModuleTargetVersion {
 
-    [CmdletBinding()]
-    param (
-        [Parameter()]
-        [string] $ModuleRelativePath
-    )
+  [CmdletBinding()]
+  param (
+    [Parameter()]
+    [string] $ModuleRelativePath
+  )
 
-    # 1. Check if [version.json] file version property was updated (compare with previous head)
-    # IF so, we reset the patch/bug version back to 0
-    # ELSE we call Get-ModuleTargetPatchVersion to get the next patch/bug version
+  # 1. Get MAJOR and MINOR from [version.json]
 
-    # 2. Concat the version elements
+  # 2. Get PATCH
+  # Check if [version.json] file version property was updated (compare with previous head)
+  # TODO: update with diff function call
+  $versionChange = 0
+  if (-not $versionChange) {
+    # If [version.json] file version property was updated, reset the patch/bug version back to 0
+    Write-Verbose "No changes detected. Skipping publishing" -Verbose
+    $patchVersion = '0'
+  }
+  else {
+    # Otherwise calculate the patch version
 
-    # 3. Return the version
+  }
+
+  # 3. Get fill Semver as MAJOR.MINOR.PATCH
+
+  # IF so, we reset the patch/bug version back to 0
+  # ELSE we call Get-ModuleTargetPatchVersion to get the next patch/bug version
+
+  # 2. Concat the version elements
+
+  # 3. Return the version
 }
