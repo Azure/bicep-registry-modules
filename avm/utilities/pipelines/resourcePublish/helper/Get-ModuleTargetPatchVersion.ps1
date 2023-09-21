6f49701be25bf4ecd7756a8f55f7b3f22c0ff42f
Erika Gressi <erikag@microsoft.com>
2023-09-21 15:17:41 +0200

    json v update

--- avm/utilities/pipelines/resourcePublish/helper/Get-ModuleTargetPatchVersion.ps1
+++ avm/utilities/pipelines/resourcePublish/helper/Get-ModuleTargetPatchVersion.ps1
@@ -27,7 +27,7 @@ function Get-ModuleTargetPatchVersion {
   # $existingTagList = git ls-remote -t origin "$modulerelativefolderpath/$MajMinVersion*"
   if ( $existingTagList.count -eq 0 ) {
     # If first module tag, reset patch
-    Write-Verbose "No existing tag for module [$ModuleRelativeFolderPath] starting with version [MajMinVersion]" -Verbose
+    Write-Verbose "No existing tag for module [$ModuleRelativeFolderPath] starting with version [$MajMinVersion]" -Verbose
     Write-Verbose "Setting patch version to 0" -Verbose
     $patch = 0
   }
