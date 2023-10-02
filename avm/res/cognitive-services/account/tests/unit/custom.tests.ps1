ea80e02adff4ae55e51bd15fe9b0e8dcc8da856a
AlexanderSehr <asehr@hotmail.de>
2023-10-02 18:09:25 +0100

    Removed unrelated changes

--- avm/res/cognitive-services/account/tests/unit/custom.tests.ps1
+++
@@ -1,7 +0,0 @@
-#############################
-## Addition unit tests     ##
-#############################
-##
-## You can add any custom static validation tests you want here, or add them spread accross multple test files in the unit folder.
-##
-#############################
