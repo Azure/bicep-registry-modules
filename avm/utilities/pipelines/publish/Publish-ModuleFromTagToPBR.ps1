e2251be2635ee6d27b1ae013fae6ba7e067d94f2
AlexanderSehr <asehr@hotmail.de>
2023-10-02 17:17:00 +0100

    Update to latest

--- avm/utilities/pipelines/publish/Publish-ModuleFromTagToPBR.ps1
+++ avm/utilities/pipelines/publish/Publish-ModuleFromTagToPBR.ps1
@@ -1,54 +1,54 @@
 function Publish-ModuleFromTagToPBR {
 
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [string] $ModuleReleaseTagName,
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory = $true)]
+    [string] $ModuleReleaseTagName,
 
-        [Parameter(Mandatory = $true)]
-        [secureString] $PublicRegistryServer
-    )
+    [Parameter(Mandatory = $true)]
+    [secureString] $PublicRegistryServer
+  )
 
-    # Load used functions
-    . (Join-Path $PSScriptRoot 'helper' 'Get-BRMRepositoryName.ps1')
-    . (Join-Path $PSScriptRoot 'helper' 'Get-ModuleReadmeLink.ps1')
-    . (Join-Path (Split-Path $PSScriptRoot -Parent) 'sharedScripts' 'tokenReplacement' 'Convert-TokensInFileList.ps1')
+  # Load used functions
+  . (Join-Path $PSScriptRoot 'helper' 'Get-BRMRepositoryName.ps1')
+  . (Join-Path $PSScriptRoot 'helper' 'Get-ModuleReadmeLink.ps1')
+  . (Join-Path (Split-Path $PSScriptRoot -Parent) 'sharedScripts' 'tokenReplacement' 'Convert-TokensInFileList.ps1')
 
-    # TODO: Diff in between tag & tag^-1 to find modules to publish?
+  # TODO: Diff in between tag & tag^-1 to find modules to publish?
 
-    # 1. Find tag as per function input
-    $repositoryRoot = (Get-Item $PSScriptRoot).Parent.Parent.Parent.Parent.Parent
-    $targetVersion = Split-Path $ModuleReleaseTagName -Leaf
-    $moduleRelativeFolderPath = $ModuleReleaseTagName -replace "\/$targetVersion$", ''
-    $moduleFolderPath = Join-Path $repositoryRoot $moduleRelativeFolderPath
-    $moduleJsonFilePath = Join-Path $moduleFolderPath 'main.json'
+  # 1. Find tag as per function input
+  $repositoryRoot = (Get-Item $PSScriptRoot).Parent.Parent.Parent.Parent.Parent
+  $targetVersion = Split-Path $ModuleReleaseTagName -Leaf
+  $moduleRelativeFolderPath = $ModuleReleaseTagName -replace "\/$targetVersion$", ''
+  $moduleFolderPath = Join-Path $repositoryRoot $moduleRelativeFolderPath
+  $moduleJsonFilePath = Join-Path $moduleFolderPath 'main.json'
 
 
-    # 2. Get Target Published Module Name
-    $publishedModuleName = Get-BRMRepositoryName -TemplateFilePath $moduleJsonFilePath
+  # 2. Get Target Published Module Name
+  $publishedModuleName = Get-BRMRepositoryName -TemplateFilePath $moduleJsonFilePath
 
-    # 3. Get the documentation link
-    $documentationUri = Get-ModuleReadmeLink -TagName $ModuleReleaseTagName -ModuleRelativeFolderPath $moduleRelativeFolderPath
+  # 3. Get the documentation link
+  $documentationUri = Get-ModuleReadmeLink -TagName $ModuleReleaseTagName -ModuleRelativeFolderPath $moduleRelativeFolderPath
 
-    # 4. Replace telemetry version value (in JSON)
-    $tokenConfiguration = @{
-        FilePathList = @($moduleJsonFilePath)
-        Tokens       = @{
-            'moduleVersion' = $targetVersion
-        }
+  # 4. Replace telemetry version value (in JSON)
+  $tokenConfiguration = @{
+    FilePathList = @($moduleJsonFilePath)
+    Tokens       = @{
+      'moduleVersion' = $targetVersion
     }
-    $null = Convert-TokensInFileList @tokenConfiguration
+  }
+  $null = Convert-TokensInFileList @tokenConfiguration
 
-    ###################
-    ## 5.  Publish   ##
-    ###################
-    $plainPublicRegistryServer = ConvertFrom-SecureString $PublicRegistryServer -AsPlainText
+  ###################
+  ## 5.  Publish   ##
+  ###################
+  $plainPublicRegistryServer = ConvertFrom-SecureString $PublicRegistryServer -AsPlainText
 
-    $publishInput = @(
-        $moduleJsonFilePath
-        '--target', ("br:{0}/public/bicep/{1}:{2}" -f $plainPublicRegistryServer, $publishedModuleName, $targetVersion)
-        '--documentationUri', $documentationUri
-        '--force'
-    )
-    # bicep publish @publishInput
+  $publishInput = @(
+    $moduleJsonFilePath
+    '--target', ("br:{0}/public/bicep/{1}:{2}" -f $plainPublicRegistryServer, $publishedModuleName, $targetVersion)
+    '--documentationUri', $documentationUri
+    '--force'
+  )
+  # bicep publish @publishInput
 }
