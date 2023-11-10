6eaffc496dc1921bfdbdef20588496e143cde20f
Alexander Sehr <ASehr@hotmail.de>
2023-11-11 00:43:29 +0100

    Update avm/utilities/pipelines/staticValidation/compliance/module.tests.ps1
    Co-authored-by: Erika Gressi <56914614+eriqua@users.noreply.github.com>

--- avm/utilities/pipelines/staticValidation/compliance/module.tests.ps1
+++ avm/utilities/pipelines/staticValidation/compliance/module.tests.ps1
@@ -1009,7 +1009,7 @@ Describe 'Module tests' -Tag 'Module' {
 
       if ($templateFileContent.parameters.Keys -contains $parameterName) {
         $templateFileContent.parameters.$parameterName.Keys | Should -Contain '$ref' -Because "the [$parameterName] parameter should use a user-defined type. For information please review the [AVM Specs]($link)."
-        $templateFileContent.parameters.$parameterName.'$ref' | Should -Be "#/definitions/$udtName" -Because "the [$parameterName] parameter should use a user-defined type [$udtName]. For for information please review the [AVM Specs]($link)."
+        $templateFileContent.parameters.$parameterName.'$ref' | Should -Be "#/definitions/$udtName" -Because "the [$parameterName] parameter should use a user-defined type [$udtName]. For information please review the [AVM Specs]($link)."
 
         if (-not [String]::IsNullOrEmpty($expectedUdtUrl)) {
           $implementedSchemaStartIndex = $templateFileContentBicep.IndexOf("type $udtName = {")
