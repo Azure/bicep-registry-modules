d16099e1908982d89a17608afce9f64e8f1338a6
AlexanderSehr <asehr@hotmail.de>
2023-11-26 11:16:05 +0100

    Added publish from tag pipeline

--- avm/utilities/pipelines/publish/Publish-ModuleFromTagToPBR.ps1
+++ avm/utilities/pipelines/publish/Publish-ModuleFromTagToPBR.ps1
@@ -1,11 +1,29 @@
+<#
+.SYNOPSIS
+Publish a module based on the provided git tag
+
+.DESCRIPTION
+Publish a module based on the provided git tag
+
+.PARAMETER ModuleReleaseTagName
+Mandatory. The git tag to identify the module with & publish its code state of
+
+.PARAMETER PublicRegistryServer
+Mandatory. The public registry server.
+
+.EXAMPLE
+Publish-ModuleFromTagToPBR -ModuleReleaseTagName 'avm/res/key-vault/vault/0.3.0' -PublicRegistryServer (ConvertTo-SecureString 'myServer' -AsPlainText -Force)
+
+Publish the module 'avm/res/key-vault/vault' of git tag 'avm/res/key-vault/vault/0.3.0' to the public registry server 'myServer'
+#>
 function Publish-ModuleFromTagToPBR {
 
-  [CmdletBinding()]
+  [CmdletBinding(SupportsShouldProcess)]
   param (
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory)]
     [string] $ModuleReleaseTagName,
 
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory)]
     [secureString] $PublicRegistryServer
   )
 
@@ -14,53 +32,31 @@ function Publish-ModuleFromTagToPBR {
   . (Join-Path $PSScriptRoot 'helper' 'Get-ModuleReadmeLink.ps1')
   . (Join-Path (Split-Path $PSScriptRoot -Parent) 'sharedScripts' 'tokenReplacement' 'Convert-TokensInFileList.ps1')
 
-  # TODO: Diff in between tag & tag^-1 to find modules to publish?
-
   # 1. Find tag as per function input
-  $repositoryRoot = (Get-Item $PSScriptRoot).Parent.Parent.Parent.Parent.Parent
+  $repositoryRoot = (Get-Item $PSScriptRoot).Parent.Parent.Parent.Parent.Parent.FullName
   $targetVersion = Split-Path $ModuleReleaseTagName -Leaf
   $moduleRelativeFolderPath = $ModuleReleaseTagName -replace "\/$targetVersion$", ''
   $moduleFolderPath = Join-Path $repositoryRoot $moduleRelativeFolderPath
   $moduleJsonFilePath = Join-Path $moduleFolderPath 'main.json'
 
-
-  # 2. Get Target Published Module Name
-  $publishedModuleName = Get-BRMRepositoryName -TemplateFilePath $moduleJsonFilePath
-
-  # 3. Get the documentation link
-  $documentationUri = Get-ModuleReadmeLink -TagName $ModuleReleaseTagName -ModuleRelativeFolderPath $moduleRelativeFolderPath
-
-  # 4. Replace telemetry version value (in JSON)
-  $tokenConfiguration = @{
-    FilePathList   = @($moduleJsonFilePath)
-    AbsoluteTokens = @{
-      '-..--..-' = $targetVersion
-    }
-  }
-  $null = Convert-TokensInFileList @tokenConfiguration
-
-  # Double-check that tokens are correctly replaced
-  $templateContent = Get-Content -Path $moduleJsonFilePath
-  $incorrectLines = @()
-  for ($index = 0; $index -lt $templateContent.Count; $index++) {
-    if ($templateContent[$index] -match '-..--..-') {
-      $incorrectLines += ('You have the token [{0}] in line [{1}] of file [{2}]. Please seek advice from the AVM team.' -f $matches[0], ($index + 1), $moduleJsonFilePath)
-    }
-  }
-  if ($incorrectLines) {
-    throw ($incorrectLines | ConvertTo-Json)
-  }
+  # 2. Get the documentation link
+  $documentationUri = Get-ModuleReadmeLink -TagName $ModuleReleaseTagName -ModuleFolderPath $moduleFolderPath
 
   ###################
-  ## 5.  Publish   ##
+  ## 3.  Publish   ##
   ###################
   $plainPublicRegistryServer = ConvertFrom-SecureString $PublicRegistryServer -AsPlainText
 
   $publishInput = @(
     $moduleJsonFilePath
-    '--target', ("br:{0}/public/bicep/{1}:{2}" -f $plainPublicRegistryServer, $publishedModuleName, $targetVersion)
+    '--target', ("br:{0}/public/bicep/{1}:{2}" -f $plainPublicRegistryServer, $moduleRelativeFolderPath, $targetVersion)
     '--documentationUri', $documentationUri
     '--force'
   )
-  # bicep publish @publishInput
+
+  Write-Verbose "Publish Input:`n $($publishInput | ConvertTo-Json -Depth 10)" -Verbose
+
+  if ($PSCmdlet.ShouldProcess("Module of tag [$ModuleReleaseTagName]", "Publish")) {
+    # bicep publish @publishInput
+  }
 }
