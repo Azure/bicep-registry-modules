c2ffd12fc2bbfd7bd1ed6f131362d40fbe76e6ea
Rainer Halanek <rahalan@microsoft.com>
2024-01-03 16:51:13 +0100

    filter for AVM module issues

--- avm/utilities/pipelines/platform/Set-AvmGitHubIssueOwnerConfig.ps1
+++ avm/utilities/pipelines/platform/Set-AvmGitHubIssueOwnerConfig.ps1
@@ -92,48 +92,51 @@ function Set-AvmGitHubIssueOwnerConfig {
   )
 
   $issue = gh issue view $IssueUrl.Replace('api.', '').Replace('repos/', '') --json 'author,title,url,body,comments' --repo $Repo | ConvertFrom-Json -Depth 100
-  $moduleName = ($issue.body.Split("`n") -match "avm/(?:res|ptn)")[0].Trim().Replace(' ', '')
 
-  if ([string]::IsNullOrEmpty($moduleName)) {
-    throw 'No valid module name was found in the issue.'
-  }
+  if ($issue.title.StartsWith('[AVM Module Issue]')) {
+    $moduleName = ($issue.body.Split("`n") -match "avm/(?:res|ptn)")[0].Trim().Replace(' ', '')
 
-  $moduleIndex = $moduleName.StartsWith("avm/res") ? "Bicep-Resource" : "Bicep-Pattern"
-  $module = Get-AvmCsvData -ModuleIndex $moduleIndex | Where-Object ModuleName -eq $moduleName
+    if ([string]::IsNullOrEmpty($moduleName)) {
+      throw 'No valid module name was found in the issue.'
+    }
 
-  if ([string]::IsNullOrEmpty($module)) {
-    throw "Module $moduleName was not found in $moduleIndex CSV file."
-  }
+    $moduleIndex = $moduleName.StartsWith("avm/res") ? "Bicep-Resource" : "Bicep-Pattern"
+    $module = Get-AvmCsvData -ModuleIndex $moduleIndex | Where-Object ModuleName -eq $moduleName
 
-  $reply = @"
+    if ([string]::IsNullOrEmpty($module)) {
+      throw "Module $moduleName was not found in $moduleIndex CSV file."
+    }
+
+    $reply = @"
 @$($issue.author.login), thanks for submitting this issue for the ``$moduleName`` module!
 
 A member of the @azure/$($module.ModuleOwnersGHTeam) or @azure/$($module.ModuleContributorsGHTeam) team will review it soon!
 "@
 
-  if ($PSCmdlet.ShouldProcess("attention label to issue [$issue.title]", 'Add')) {
-    # add labels
-    gh issue edit $issue.url --add-label "Needs: Attention :wave:" --repo $Repo
-  }
+    if ($PSCmdlet.ShouldProcess("attention label to issue [$issue.title]", 'Add')) {
+      # add labels
+      gh issue edit $issue.url --add-label "Needs: Attention :wave:" --repo $Repo
+    }
 
-  if ($PSCmdlet.ShouldProcess("class label to issue [$issue.title]", 'Add')) {
-    gh issue edit $issue.url --add-label ($moduleIndex -eq "Bicep-Resource" ? "Class: Resource Module :package:" : "Class: Pattern Module :package:") --repo $Repo
-  }
+    if ($PSCmdlet.ShouldProcess("class label to issue [$issue.title]", 'Add')) {
+      gh issue edit $issue.url --add-label ($moduleIndex -eq "Bicep-Resource" ? "Class: Resource Module :package:" : "Class: Pattern Module :package:") --repo $Repo
+    }
 
-  if ($PSCmdlet.ShouldProcess("reply comment to issue [$issue.title]", 'Add')) {
-    # write comment
-    gh issue comment $issue.url --body $reply --repo $Repo
-  }
-
-  if ($PSCmdlet.ShouldProcess(("owner [{0}] to issue [$issue.title]" -f $module.PrimaryModuleOwnerGHHandle), 'Assign')) {
-    # assign owner
-    $assign = gh issue edit $issue.url --add-assignee $module.PrimaryModuleOwnerGHHandle --repo $Repo
-  }
-
-  if ([String]::IsNullOrEmpty($assign)) {
-    $reply = "This issue couldn't be assigend to $($module.PrimaryModuleOwnerGHHandle), because no such users exists."
-    if ($PSCmdlet.ShouldProcess("missing user comment to issue [$issue.title]", 'Add')) {
+    if ($PSCmdlet.ShouldProcess("reply comment to issue [$issue.title]", 'Add')) {
+      # write comment
       gh issue comment $issue.url --body $reply --repo $Repo
+    }
+
+    if ($PSCmdlet.ShouldProcess(("owner [{0}] to issue [$issue.title]" -f $module.PrimaryModuleOwnerGHHandle), 'Assign')) {
+      # assign owner
+      $assign = gh issue edit $issue.url --add-assignee $module.PrimaryModuleOwnerGHHandle --repo $Repo
+    }
+
+    if ([String]::IsNullOrEmpty($assign)) {
+      $reply = "This issue couldn't be assigend to $($module.PrimaryModuleOwnerGHHandle), because no such users exists."
+      if ($PSCmdlet.ShouldProcess("missing user comment to issue [$issue.title]", 'Add')) {
+        gh issue comment $issue.url --body $reply --repo $Repo
+      }
     }
   }
 
