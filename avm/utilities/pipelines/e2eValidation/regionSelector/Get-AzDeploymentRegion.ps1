280d9c1ce8164639012ca73790c3f4289493006b
ChrisSidebotham-MSFT <48600046+ChrisSidebotham@users.noreply.github.com>
2024-01-18 13:02:12 +0000

    WIP

--- avm/utilities/pipelines/e2eValidation/regionSelector/Get-AzDeploymentRegion.ps1
+++ avm/utilities/pipelines/e2eValidation/regionSelector/Get-AzDeploymentRegion.ps1
@@ -34,17 +34,19 @@ function Get-AzDeploymentRegion {
   . (Join-Path $RepoRoot 'avm' 'utilities' 'pipelines' 'sharedScripts' 'helper' 'Get-SpecsAlignedResourceName.ps1')
   $fullModuleIdentifier = ($moduleRoot -split '[\/|\\]{1}avm[\/|\\]{1}(res|ptn)[\/|\\]{1}')[2] -replace '\\', '/'
   Write-Verbose "Full module identifier: $fullModuleIdentifier"
-  $formattedResourceType = Get-SpecsAlignedResourceName -ResourceIdentifier $FullModuleIdentifier -Verbose
+  $formattedResourceType = Get-SpecsAlignedResourceName -ResourceIdentifier $fullModuleIdentifier -Verbose
+  Write-Verbose "Formatted resource type: $formattedResourceType"
 
-  $type = ($fullModuleIdentifier -split '[\/|\\]{1}')[2]
+  $type = ($formattedResourceType -split '[\/|\\]{1}')[0]
   Write-Verbose "Resource type: $type"
-  $resource = ($fullModuleIdentifier -split '[\/|\\]{1}')[3]
+  $resource = ($formattedResourceType -split '[\/|\\]{1}')[1]
   Write-Verbose "Resource: $resource"
 
   $controlledRegionList = Get-Content -Path $RepoRoot/avm/regions.json -Raw | ConvertFrom-Json
 
-  if ($controlledRegionList | Get-Member -Name $type) {
-    $recommendedRegions += $controlledRegionList[$formattedResourceType]
+  if ($controlledRegionList | Get-Member -Name $formattedResourceType) {
+    $controlledRegions += $controlledRegionList."$($formattedResourceType)"
+    $regionList = Get-AzLocation | Where-Object { $_.Location -in $controlledRegions }
   }
   else {
     $providerLocations = (Get-AzResourceProvider | Where-Object { $_.ProviderNamespace -eq $type }).ResourceTypes | Where-Object { $_.ResourceTypeName -eq $resource }
@@ -57,13 +59,15 @@ function Get-AzDeploymentRegion {
     $regionList = Get-AzLocation | Where-Object { $_.Location -in $defaultRegions }
   }
 
+  Write-Verbose "Region list: $($regionList.Location | ConvertTo-Json)"
+
   # Filter regions where RegionCategory is 'Recommended' and not in the excluded list
   $recommendedRegions = $regionList | Where-Object { $_.RegionCategory -eq "Recommended" }
   Write-Verbose "Recommended regions: $($recommendedRegions.Location | ConvertTo-Json)"
 
   if ($usePairedRegionsOnly) {
     # Filter regions where PairedRegionName is not null
-    $recommendedRegions = $recommendedRegions | Where-Object { -not ([array]::IsNullOrEmpty($_.PairedRegion)) }
+    $recommendedRegions = $recommendedRegions | Where-Object { $_.PairedRegion -ne "{}" }
     Write-Verbose "Paired regions only: $($recommendedRegions.Location | ConvertTo-Json)"
   }
 
