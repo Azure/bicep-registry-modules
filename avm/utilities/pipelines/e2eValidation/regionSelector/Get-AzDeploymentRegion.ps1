46eca5ac14143dd53ebfcc998902357043f4b2c1
ChrisSidebotham-MSFT <48600046+ChrisSidebotham@users.noreply.github.com>
2024-01-18 11:41:24 +0000

    Add regions.json and update Get-AzDeploymentRegion.ps1

--- avm/utilities/pipelines/e2eValidation/regionSelector/Get-AzDeploymentRegion.ps1
+++ avm/utilities/pipelines/e2eValidation/regionSelector/Get-AzDeploymentRegion.ps1
@@ -5,90 +5,59 @@ Set the deployment region for the module deployment
 .DESCRIPTION
 This script is used to set the deployment region for the module deployment.
 
-.PARAMETER excludedRegions
-Optional. The regions to exclude from the recommended regions list.
-
 .PARAMETER usePairedRegionsOnly
 Optional. If set, only paired regions will be returned.
 
-.PARAMETER useKnownRegionsOnly
+.PARAMETER moduleRoot
 Optional. If set, only known regions will be returned.
 
-.PARAMETER knownRegionList
-Optional. The list of known regions to use when useKnownRegionsOnly is set. ARM Deployment are limited to 10 active deployment regions per subscription.
-
 .EXAMPLE
-Get-AzDeploymentRegion -excludedRegions @("West Europe", "Brazil South", "West US 2", "East US 2", "South Central US") -usePairedRegionsOnly -useKnownRegionsOnly
-
-Get the recommended regions using known regions excluding the regions specified in the excludedRegions parameter and only paired regions.
-
-.EXAMPLE
-Get-AzDeploymentRegion -excludedRegions @("West Europe", "Brazil South", "West US 2", "East US 2", "South Central US") -usePairedRegionsOnly
-
-Get the recommended regions excluding the regions specified in the excludedRegions parameter and only paired regions.
-
-.EXAMPLE
-Get-AzDeploymentRegion -excludedRegions @("West Europe", "Brazil South", "West US 2", "East US 2", "South Central US")
-
-Get the recommended regions excluding the regions specified in the excludedRegions parameter.
-
-.EXAMPLE
-Get-AzDeploymentRegion -usePairedRegionsOnly
+Get-AzDeploymentRegion -usePairedRegionsOnly -ModuleRoot $ModuleRoot
 
 Get the recommended paired regions.
 #>
 
 function Get-AzDeploymentRegion {
   param (
-    [Parameter(Mandatory = $false)]
-    [array] $excludedRegions = @(
-      # "westeurope",
-      "westus2",
-      "eastus2",
-      "centralus",
-      "southcentralus",
-      "brazilsouth",
-      "koreacentral",
-      "qatarcentral"
-    ),
 
     [Parameter(Mandatory = $false)]
     [cmdletbinding()]
     [switch] $usePairedRegionsOnly,
 
-    [Parameter(Mandatory = $false)]
-    [cmdletbinding()]
-    [switch] $useKnownRegionsOnly,
-
-    [Parameter(Mandatory = $false)]
-    [array] $knownRegionList = @(
-      "westeurope"
-    )
+    [Parameter(Mandatory = $true)]
+    [string] $ModuleRoot
   )
 
-  $allRegions = Get-AzLocation -ExtendedLocation $true
+  . (Join-Path $PSScriptRoot 'helper' 'Get-SpecsAlignedResourceName.ps1')
+  $fullModuleIdentifier = ($moduleRoot -split '[\/|\\]{1}avm[\/|\\]{1}(res|ptn)[\/|\\]{1}')[2] -replace '\\', '/'
+  $formattedResourceType = Get-SpecsAlignedResourceName -ResourceIdentifier $FullModuleIdentifier
 
-  # Filter regions where Location is in the known regions list
-  if ($useKnownRegionsOnly) {
-    $regionList = $allRegions | Where-Object { $_.Location -in $knownRegionList }
+  $type = ($fullModuleIdentifier -split '[\/|\\]{1}')[2]
+  $resource = ($fullModuleIdentifier -split '[\/|\\]{1}')[3]
+
+  $controlledRegionList = Get-Content -Path $PSScriptRoot/regions.json -Raw | ConvertFrom-Json
+
+  if ($controlledRegionList | Get-Member -Name $type) {
+    $recommendedRegions += $controlledRegionList[$formattedResourceType]
   }
   else {
-    $regionList = $allRegions
+    $providerLocations = (Get-AzResourceProvider | Where-Object { $_.ProviderNamespace -eq $type }).ResourceTypes | Where-Object { $_.ResourceTypeName -eq $resource }
+    foreach ($location in $controlledRegionList.default ) {
+      if ($providerLocations.Locations -contains $location) {
+        $defaultRegions += $location
+      }
+    }
+
+    $regionList = Get-AzLocation | Where-Object { $_.Location -in $defaultRegions }
   }
 
-
-  $exclusions = @()
-  $exclusions = $excludedRegions | Where-Object { $_ -in ($_ -like '* *' ? $regionList.DisplayName : $regionList.Location) }
-  Write-Verbose ('Excluding regions: {0}' -f ($exclusions | ConvertTo-Json)) -Verbose
-
-
   # Filter regions where RegionCategory is 'Recommended' and not in the excluded list
-  $recommendedRegions = $regionList | Where-Object { $_.RegionCategory -eq "Recommended" -and $_.Location -notin $exclusions }
+  $recommendedRegions = $regionList | Where-Object { $_.RegionCategory -eq "Recommended" }
   Write-Verbose "Recommended regions: $($recommendedRegions.Location | ConvertTo-Json)"
 
   if ($usePairedRegionsOnly) {
     # Filter regions where PairedRegionName is not null
-    $recommendedRegions = $recommendedRegions | Where-Object { $null -ne $_.PairedRegion }
+    $recommendedRegions = $recommendedRegions | Where-Object { -not ([array]::IsNullOrEmpty($_.PairedRegion)) }
     Write-Verbose "Paired regions only: $($recommendedRegions.Location | ConvertTo-Json)"
   }
 
