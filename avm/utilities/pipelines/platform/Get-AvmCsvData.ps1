85ea1e60fce17eda61d1f98dfd08cb6454f59db3
Rainer Halanek <61878316+rahalan@users.noreply.github.com>
2024-01-22 12:42:00 +0100

    Update avm/utilities/pipelines/platform/Get-AvmCsvData.ps1
    Co-authored-by: Alexander Sehr <ASehr@hotmail.de>

--- avm/utilities/pipelines/platform/Get-AvmCsvData.ps1
+++ avm/utilities/pipelines/platform/Get-AvmCsvData.ps1
@@ -6,7 +6,7 @@ Parses AVM module CSV file
 Depending on the parameter, the correct CSV file will be parsed and returned a an object
 
 .PARAMETER ModuleIndex
-Type of CSV file, that should be parsed ('Bicep-Resource', 'Bicep-Pattern')
+Type of CSV file, that should be parsed. E.g., 'Bicep-Resource'
 
 .EXAMPLE
 Next line will parse the AVM Bicep modules
