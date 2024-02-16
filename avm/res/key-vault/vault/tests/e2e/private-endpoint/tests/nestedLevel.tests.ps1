c2637f384edb9d36b4627b535d74a0576b39b31a
AlexanderSehr <asehr@hotmail.de>
2024-02-16 22:24:29 +0100

    Update to latest

---
+++ avm/res/key-vault/vault/tests/e2e/private-endpoint/tests/nestedLevel.tests.ps1
@@ -0,0 +1,24 @@
+######################################
+## Additional post-deployment tests ##
+######################################
+##
+## You can add any custom post-deployment validation tests you want here, or add them spread accross multiple test files in the test case folder.
+##
+###########################
+
+param (
+    [Parameter(Mandatory)]
+    [hashtable] $DeploymentOutputs
+)
+
+Describe 'Validate key vault' {
+
+    It 'Public endpoint disabled' {
+
+        $keyVaultResourceId = $DeploymentOutputs.resourceId
+
+        $deployedResource = Get-AzResource -ResourceId $keyVaultResourceId
+
+        $deployedResource.Properties.publicNetworkAccess | Should -Be 'Disabled'
+    }
+}
---
+++ avm/res/key-vault/vault/tests/e2e/private-endpoint/topLevel.tests.ps1
@@ -0,0 +1,41 @@
+######################################
+## Additional post-deployment tests ##
+######################################
+##
+## You can add any custom post-deployment validation tests you want here, or add them spread accross multiple test files in the test case folder.
+##
+###########################
+
+param (
+    [Parameter(Mandatory)]
+    [string] $ModuleTestFolderPath,
+
+    [Parameter(Mandatory)]
+    [hashtable] $DeploymentOutputs
+)
+
+Describe 'Validate private endpoint deployment' {
+
+    Context 'Validate sucessful deployment' {
+
+        It 'Private endpoints should be deployed' {
+
+            $keyVaultResourceId = $DeploymentOutputs.resourceId
+            $testResourceGroup = ($keyVaultResourceId -split '\/')[4]
+            $deployedPrivateEndpoints = Get-AzPrivateEndpoint -ResourceGroupName $testResourceGroup
+            $deployedPrivateEndpoints.Count | Should -BeGreaterThan 0
+        }
+
+        It 'Private endpoint should have role assignment' {
+
+            $keyVaultResourceId = $DeploymentOutputs.resourceId
+            $testResourceGroup = ($keyVaultResourceId -split '\/')[4]
+            $deployedPrivateEndpoints = Get-AzPrivateEndpoint -ResourceGroupName $testResourceGroup
+            $firstPrivateEndpointResourceId = $deployedPrivateEndpoints[0].Id
+            $firstPrivateEndpointName = ($firstPrivateEndpointResourceId -split '\/')[-1]
+
+            $roleAssignments = Get-AzRoleAssignment -ResourceName $firstPrivateEndpointName -ResourceType 'Microsoft.Network/privateEndpoints' -ResourceGroupName $testResourceGroup
+            $roleAssignments | Should -BeGreaterThan 0
+        }
+    }
+}
