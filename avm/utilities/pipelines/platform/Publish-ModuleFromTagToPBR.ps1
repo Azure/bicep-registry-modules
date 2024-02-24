a902b1f707cf1af2d2a579cfd68481a77a606e4d
Erika Gressi <erikag@microsoft.com>
2024-02-24 18:42:55 +0100

    comment all

--- avm/utilities/pipelines/platform/Publish-ModuleFromTagToPBR.ps1
+++ avm/utilities/pipelines/platform/Publish-ModuleFromTagToPBR.ps1
@@ -35,66 +35,66 @@ function Publish-ModuleFromTagToPBR {
   )
 
   Write-Verbose "1 [$ModuleReleaseTagName]" -Verbose
-  # Load used functions
-  . (Join-Path $RepoRoot 'avm' 'utilities' 'pipelines' 'publish' 'helper' 'Get-ModuleReadmeLink.ps1')
+  # # Load used functions
+  # . (Join-Path $RepoRoot 'avm' 'utilities' 'pipelines' 'publish' 'helper' 'Get-ModuleReadmeLink.ps1')
 
-  Write-Verbose "2 [$ModuleReleaseTagName]" -Verbose
-  # 1. Extract information from the tag
-  $targetVersion = Split-Path $ModuleReleaseTagName -Leaf
-  Write-Verbose "1 [$targetVersion]" -Verbose
-  $moduleRelativeFolderPath = $ModuleReleaseTagName -replace "\/$targetVersion$", ''
-  Write-Verbose "1 [$moduleRelativeFolderPath]" -Verbose
-  $moduleFolderPath = Join-Path $repositoryRoot $moduleRelativeFolderPath
-  Write-Verbose "1 [$moduleFolderPath]" -Verbose
-  $moduleBicepFilePath = Join-Path $moduleFolderPath 'main.bicep'
-  Write-Verbose "Determined Bicep template path [$moduleBicepFilePath]"
+  # Write-Verbose "2 [$ModuleReleaseTagName]" -Verbose
+  # # 1. Extract information from the tag
+  # $targetVersion = Split-Path $ModuleReleaseTagName -Leaf
+  # Write-Verbose "1 [$targetVersion]" -Verbose
+  # $moduleRelativeFolderPath = $ModuleReleaseTagName -replace "\/$targetVersion$", ''
+  # Write-Verbose "1 [$moduleRelativeFolderPath]" -Verbose
+  # $moduleFolderPath = Join-Path $repositoryRoot $moduleRelativeFolderPath
+  # Write-Verbose "1 [$moduleFolderPath]" -Verbose
+  # $moduleBicepFilePath = Join-Path $moduleFolderPath 'main.bicep'
+  # Write-Verbose "Determined Bicep template path [$moduleBicepFilePath]"
 
-  # 2. Get the documentation link
-  $documentationUri = Get-ModuleReadmeLink -TagName $ModuleReleaseTagName -ModuleFolderPath $moduleFolderPath
-  Write-Verbose "Determined documentation URI [$documentationUri]"
+  # # 2. Get the documentation link
+  # $documentationUri = Get-ModuleReadmeLink -TagName $ModuleReleaseTagName -ModuleFolderPath $moduleFolderPath
+  # Write-Verbose "Determined documentation URI [$documentationUri]"
 
-  # 3. Replace telemetry version value (in Bicep)
-  $tokenConfiguration = @{
-    FilePathList   = @($moduleBicepFilePath)
-    AbsoluteTokens = @{
-      '-..--..-' = $targetVersion
-    }
-  }
-  Write-Verbose "Convert Tokens Input:`n $($tokenConfiguration | ConvertTo-Json -Depth 10)" -Verbose
-  $null = Convert-TokensInFileList @tokenConfiguration
+  # # 3. Replace telemetry version value (in Bicep)
+  # $tokenConfiguration = @{
+  #   FilePathList   = @($moduleBicepFilePath)
+  #   AbsoluteTokens = @{
+  #     '-..--..-' = $targetVersion
+  #   }
+  # }
+  # Write-Verbose "Convert Tokens Input:`n $($tokenConfiguration | ConvertTo-Json -Depth 10)" -Verbose
+  # $null = Convert-TokensInFileList @tokenConfiguration
 
-  # Double-check that tokens are correctly replaced
-  $templateContent = Get-Content -Path $moduleBicepFilePath
-  $incorrectLines = @()
-  for ($index = 0; $index -lt $templateContent.Count; $index++) {
-    if ($templateContent[$index] -match '-..--..-') {
-      $incorrectLines += ('You have the token [{0}] in line [{1}] of file [{2}]. Please seek advice from the AVM team.' -f $matches[0], ($index + 1), $moduleBicepFilePath)
-    }
-  }
-  if ($incorrectLines) {
-    throw ($incorrectLines | ConvertTo-Json)
-  }
-  ###################
-  ## 4.  Publish   ##
-  ###################
-  $plainPublicRegistryServer = ConvertFrom-SecureString $PublicRegistryServer -AsPlainText
+  # # Double-check that tokens are correctly replaced
+  # $templateContent = Get-Content -Path $moduleBicepFilePath
+  # $incorrectLines = @()
+  # for ($index = 0; $index -lt $templateContent.Count; $index++) {
+  #   if ($templateContent[$index] -match '-..--..-') {
+  #     $incorrectLines += ('You have the token [{0}] in line [{1}] of file [{2}]. Please seek advice from the AVM team.' -f $matches[0], ($index + 1), $moduleBicepFilePath)
+  #   }
+  # }
+  # if ($incorrectLines) {
+  #   throw ($incorrectLines | ConvertTo-Json)
+  # }
+  # ###################
+  # ## 4.  Publish   ##
+  # ###################
+  # $plainPublicRegistryServer = ConvertFrom-SecureString $PublicRegistryServer -AsPlainText
 
-  $publishInput = @(
-    $moduleBicepFilePath
-    '--target', ("br:{0}/public/bicep/{1}:{2}" -f $plainPublicRegistryServer, $moduleRelativeFolderPath, $targetVersion)
-    '--documentationUri', $documentationUri
-    '--with-source'
-    '--force'
-  )
+  # $publishInput = @(
+  #   $moduleBicepFilePath
+  #   '--target', ("br:{0}/public/bicep/{1}:{2}" -f $plainPublicRegistryServer, $moduleRelativeFolderPath, $targetVersion)
+  #   '--documentationUri', $documentationUri
+  #   '--with-source'
+  #   '--force'
+  # )
 
-  Write-Verbose "Publish Input:`n $($publishInput | ConvertTo-Json -Depth 10)" -Verbose
+  # Write-Verbose "Publish Input:`n $($publishInput | ConvertTo-Json -Depth 10)" -Verbose
 
-  if ($PSCmdlet.ShouldProcess("Module of tag [$ModuleReleaseTagName]", "Publish")) {
-    bicep publish @publishInput
-  }
+  # if ($PSCmdlet.ShouldProcess("Module of tag [$ModuleReleaseTagName]", "Publish")) {
+  #   bicep publish @publishInput
+  # }
 
-  return @{
-    version             = $targetVersion
-    publishedModuleName = $moduleRelativeFolderPath
-  }
+  # return @{
+  #   version             = $targetVersion
+  #   publishedModuleName = $moduleRelativeFolderPath
+  # }
 }
