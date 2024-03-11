16fb3ef1e82ef094d9d05c59d8686d55d19e5335
Erika Gressi <erikag@microsoft.com>
2024-03-11 14:50:10 +0100

    simplify unique expressions

--- avm/utilities/pipelines/staticValidation/psrule/Set-PSRuleGitHubOutput.ps1
+++ avm/utilities/pipelines/staticValidation/psrule/Set-PSRuleGitHubOutput.ps1
@@ -57,15 +57,17 @@ function Set-PSRuleGitHubOutput {
         # $passedRules += $results | Where-Object { $_.Outcome -EQ 'Pass' } | Sort-Object -Property 'RuleName' -Unique
         # $failedRules += $results | Where-Object { $_.Outcome -EQ 'Fail' } | Sort-Object -Property 'RuleName' -Unique
 
-        # $passedRules += $results | Where-Object { $_.Outcome -EQ 'Pass' } | Sort-Object -Property @{Expression = "TargetName"; Descending = $true},
+        # $passedRules += $results |
+        #     Where-Object { $_.Outcome -EQ 'Pass' } |
+        #     Sort-Object -Property @{Expression = "TargetName"}, @{Expression = "RuleName"} -Unique
+
+        # $failedRules += $results |
+        #     Where-Object { $_.Outcome -EQ 'Fail' } | Sort-Object -Property @{Expression = "TargetName"; Descending = $true},
         #                   @{Expression = "RuleName"; Descending = $false} -Unique
 
-        # $failedRules += $results | Where-Object { $_.Outcome -EQ 'Fail' } | Sort-Object -Property @{Expression = "TargetName"; Descending = $true},
-        #                   @{Expression = "RuleName"; Descending = $false} -Unique
+        $passedRules += $results | Where-Object { $_.Outcome -EQ 'Pass' } | Sort-Object -Property {'TargetName' 'RuleName'} -Unique
 
-        $passedRules += $results | Where-Object { $_.Outcome -EQ 'Pass' } | Sort-Object -Property {$_.TargetName - $_.RuleName} -Unique
-
-        $failedRules += $results | Where-Object { $_.Outcome -EQ 'Fail' } | Sort-Object -Property {$_.TargetName - $_.RuleName} -Unique
+        $failedRules += $results | Where-Object { $_.Outcome -EQ 'Fail' } | Sort-Object -Property {'TargetName' 'RuleName'} -Unique
 
         Write-Verbose ('total [{0}]' -f $results.Count ) -Verbose
         Write-Verbose ('passedRules [{0}]' -f $passedRules.Count ) -Verbose
