8d06b8fbc7c4d883c9d6efa3f73a8c7379c34438
Rainer Halanek <rahalan@microsoft.com>
2024-03-15 17:45:41 +0100

    move function out into seperate file (again)

--- avm/utilities/pipelines/platform/Set-AvmGitHubIssueOwnerConfig.ps1
+++ avm/utilities/pipelines/platform/Set-AvmGitHubIssueOwnerConfig.ps1
@@ -1,67 +1,5 @@
 <#
 .SYNOPSIS
-Parses AVM module CSV file
-
-.DESCRIPTION
-Depending on the parameter, the correct CSV file will be parsed and returned a an object
-
-.PARAMETER ModuleIndex
-Type of CSV file, that should be parsed ('Bicep-Resource', 'Bicep-Pattern')
-
-.EXAMPLE
-Next line will parse the AVM Bicep modules
-Get-AvmCsvData -ModuleIndex 'Bicep-Resource'
-
-#>
-Function Get-AvmCsvData {
-  [CmdletBinding()]
-  param (
-    [Parameter(Mandatory)]
-    [ValidateSet('Bicep-Resource', 'Bicep-Pattern')]
-    [string] $ModuleIndex
-  )
-
-  # CSV file URLs
-  $BicepResourceUrl = "https://aka.ms/avm/index/bicep/res/csv"
-  $BicepPatternUrl = "https://aka.ms/avm/index/bicep/ptn/csv"
-
-  # Retrieve the CSV file
-  switch ($ModuleIndex) {
-    'Bicep-Resource' {
-      try {
-        $unfilteredCSV = Invoke-WebRequest -Uri $BicepResourceUrl
-      }
-      catch {
-        Write-Error "Unable to retrieve CSV file - Check network connection."
-      }
-    }
-    'Bicep-Pattern' {
-      try {
-        $unfilteredCSV = Invoke-WebRequest -Uri $BicepPatternUrl
-      }
-      catch {
-        Write-Error "Unable to retrieve CSV file - Check network connection."
-      }
-    }
-  }
-
-  # Convert the CSV content to a PowerShell object
-  $formattedBicepFullCsv = ConvertFrom-CSV $unfilteredCSV.Content
-
-  # Loop through each item in the filtered data
-  foreach ($item in $formattedBicepFullCsv) {
-    # Remove '@Azure/' from the ModuleOwnersGHTeam property
-    $item.ModuleOwnersGHTeam = $item.ModuleOwnersGHTeam -replace '@Azure/', ''
-    # Remove '@Azure/' from the ModuleContributorsGHTeam property
-    $item.ModuleContributorsGHTeam = $item.ModuleContributorsGHTeam -replace '@Azure/', ''
-  }
-
-  # Return the modified data
-  return $formattedBicepFullCsv
-}
-
-<#
-.SYNOPSIS
 Assigns issues to module owners and adds comments and labels
 
 .DESCRIPTION
@@ -88,6 +26,8 @@ function Set-AvmGitHubIssueOwnerConfig {
     [Parameter(Mandatory = $true)]
     [string] $IssueUrl
   )
+  # Load used functions
+  . (Join-Path $RepoRoot 'avm' 'utilities' 'pipelines' 'platform' 'Get-AvmCsvData.ps1')
 
   $issue = gh issue view $IssueUrl.Replace('api.', '').Replace('repos/', '') --json 'author,title,url,body,comments' --repo $Repo | ConvertFrom-Json -Depth 100
 
