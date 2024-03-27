b1777e225a55c148902ed1beb5884449d24d92a4
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2024-03-27 16:48:51 +0200

    update resource id

--- avm/ptn/lz/sub-vending/tests/e2e/vwan-spoke/pester/main.tests.ps1
+++ avm/ptn/lz/sub-vending/tests/e2e/vwan-spoke/pester/main.tests.ps1
@@ -122,7 +122,7 @@ Describe "Bicep Landing Zone (Sub) Vending Tests" {
 
   Context "Networking - Virtual WAN Hub Tests" {
     BeforeAll {
-      Select-AzSubscription -SubscriptionId "e4e7395f-dc45-411e-b425-95f75e470e16" -ErrorAction Stop
+      Select-AzSubscription -SubscriptionId "9948cae8-8c7c-4f5f-81c1-c53317cab23d" -ErrorAction Stop
       $vwanHub = $vwanHub = Get-AzVirtualHub -ResourceGroupName "rsg-blzv-perm-hubs-001" -Name "vhub-uksouth-blzv" -ErrorAction SilentlyContinue
       $vwanHubVhc = Get-AzVirtualHubVnetConnection -ResourceGroupName "rsg-blzv-perm-hubs-001" -VirtualHubName "vhub-uksouth-blzv" -Name *
     }
