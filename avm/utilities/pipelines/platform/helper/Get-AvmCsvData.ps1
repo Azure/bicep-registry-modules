a3c2c71020592304234c6c73aa8d60cb6e7d5017
Rainer Halanek <rahalan@microsoft.com>
2024-04-06 17:05:18 +0200

    use new token

--- avm/utilities/pipelines/platform/helper/Get-AvmCsvData.ps1
+++ avm/utilities/pipelines/platform/helper/Get-AvmCsvData.ps1
@@ -1,12 +1,16 @@
 <#
 .SYNOPSIS
 Parses AVM module CSV file
+
 .DESCRIPTION
 Depending on the parameter, the correct CSV file will be parsed and returned a an object
+
 .PARAMETER ModuleIndex
 Mandatory. Type of CSV file, that should be parsed ('Bicep-Resource', 'Bicep-Pattern')
+
 .EXAMPLE
 Get-AvmCsvData -ModuleIndex 'Bicep-Resource'
+
 Parse the AVM Bicep modules
 #>
 Function Get-AvmCsvData {
