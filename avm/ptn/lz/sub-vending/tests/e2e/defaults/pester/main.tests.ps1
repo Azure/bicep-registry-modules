c98811ee4a5a7e683aa5858b4c031144f4ccaf2d
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2024-04-18 15:49:47 +0200

    change sub name in tests

--- avm/ptn/lz/sub-vending/tests/e2e/defaults/pester/main.tests.ps1
+++ avm/ptn/lz/sub-vending/tests/e2e/defaults/pester/main.tests.ps1
@@ -3,7 +3,7 @@ param (
   [hashtable] $TestInputData = @{}
 )
 
-Describe "Bicep Landing Zone (Sub) Vending Tests" {
+Describe 'Bicep Landing Zone (Sub) Vending Tests' {
 
   BeforeAll {
     $subscriptionId = $TestInputData.DeploymentOutputs.createdSubId.Value
@@ -14,17 +14,17 @@ Describe "Bicep Landing Zone (Sub) Vending Tests" {
     Select-AzSubscription -subscriptionId $subscriptionId
   }
 
-  Context "Subscription Tests" {
+  Context 'Subscription Tests' {
     BeforeAll {
       $sub = Get-AzSubscription -SubscriptionId $subscriptionId -ErrorAction SilentlyContinue
     }
 
-    It "Should have a Subscription with the correct name" {
-      $sub.Name | Should -Be "sub-blzv-tests-$namePrefix-$serviceShort"
+    It 'Should have a Subscription with the correct name' {
+      $sub.Name | Should -Be "dep-sub-blzv-tests-$namePrefix-$serviceShort"
     }
 
-    It "Should have a Subscription that is enabled" {
-      $sub.State | Should -Be "Enabled"
+    It 'Should have a Subscription that is enabled' {
+      $sub.State | Should -Be 'Enabled'
     }
 
     It "Should have a Subscription with a tag key of 'namePrefix' with a value of '$namePrefix'" {
@@ -36,30 +36,30 @@ Describe "Bicep Landing Zone (Sub) Vending Tests" {
     }
 
     It "Should have a Subscription that is a child of the Management Group with the ID of 'bicep-lz-vending-automation-child'" {
-      $mgAssociation = Get-AzManagementGroupSubscription -SubscriptionId $subscriptionId -GroupId "bicep-lz-vending-automation-child" -ErrorAction SilentlyContinue
+      $mgAssociation = Get-AzManagementGroupSubscription -SubscriptionId $subscriptionId -GroupId 'bicep-lz-vending-automation-child' -ErrorAction SilentlyContinue
       $mgAssociation.Id | Should -Be "/providers/Microsoft.Management/managementGroups/bicep-lz-vending-automation-child/subscriptions/$subscriptionId"
     }
 
     It "Should have the 'Microsoft.HybridCompute', 'Microsoft.AVS' resource providers and the 'AzureServicesVm', 'ArcServerPrivateLinkPreview' resource providers features registered" {
-      $resourceProviders = @( "Microsoft.HybridCompute", "Microsoft.AVS" )
-      $resourceProvidersFeatures = @( "AzureServicesVm", "ArcServerPrivateLinkPreview" )
+      $resourceProviders = @( 'Microsoft.HybridCompute', 'Microsoft.AVS' )
+      $resourceProvidersFeatures = @( 'AzureServicesVm', 'ArcServerPrivateLinkPreview' )
       ForEach ($provider in $resourceProviders) {
-        $providerStatus = (Get-AzResourceProvider -ListAvailable | Where-Object ProviderNamespace -eq $provider).registrationState
+        $providerStatus = (Get-AzResourceProvider -ListAvailable | Where-Object ProviderNamespace -EQ $provider).registrationState
         $providerStatus | Should -BeIn @('Registered', 'Registering')
       }
 
       ForEach ($feature in $resourceProvidersFeatures) {
-        $providerFeatureStatus = (Get-AzProviderFeature -ListAvailable | Where-Object FeatureName -eq $feature).registrationState
+        $providerFeatureStatus = (Get-AzProviderFeature -ListAvailable | Where-Object FeatureName -EQ $feature).registrationState
         $providerFeatureStatus | Should -BeIn @('Registered', 'Registering', 'Pending')
       }
     }
   }
 
-  Context "Role-Based Access Control Assignment Tests" {
-    It "Should Have a Role Assignment for an known AAD Group with the Reader role directly upon the Subscription" {
+  Context 'Role-Based Access Control Assignment Tests' {
+    It 'Should Have a Role Assignment for an known AAD Group with the Reader role directly upon the Subscription' {
       $iterationCount = 0
       do {
-        $roleAssignment = Get-AzRoleAssignment -Scope "/subscriptions/$subscriptionId" -RoleDefinitionName "Reader" -ObjectId "896b1162-be44-4b28-888a-d01acc1b4271" -ErrorAction SilentlyContinue
+        $roleAssignment = Get-AzRoleAssignment -Scope "/subscriptions/$subscriptionId" -RoleDefinitionName 'Reader' -ObjectId '896b1162-be44-4b28-888a-d01acc1b4271' -ErrorAction SilentlyContinue
         if ($null -eq $roleAssignment) {
           Write-Host "Waiting for Subscription Role Assignments to be eventually consistent... Iteration: $($iterationCount)" -ForegroundColor Yellow
           Start-Sleep -Seconds 40
@@ -69,8 +69,8 @@ Describe "Bicep Landing Zone (Sub) Vending Tests" {
         $roleAssignment -ne $null -or $iterationCount -ge 10
       )
 
-      $roleAssignment.ObjectId | Should -Be "896b1162-be44-4b28-888a-d01acc1b4271"
-      $roleAssignment.RoleDefinitionName | Should -Be "Reader"
+      $roleAssignment.ObjectId | Should -Be '896b1162-be44-4b28-888a-d01acc1b4271'
+      $roleAssignment.RoleDefinitionName | Should -Be 'Reader'
       $roleAssignment.scope | Should -Be "/subscriptions/$subscriptionId"
     }
   }
