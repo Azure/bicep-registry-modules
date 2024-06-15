28dc9d4f9f7770415e22c77699ff18a344100d32
jbinko <jbinko+github@gmail.com>
2024-06-15 10:29:50 +0200

    update

--- avm/ptn/data/private-analytical-workspace/tests/e2e/log-existing/custom.tests.ps1
+++ avm/ptn/data/private-analytical-workspace/tests/e2e/log-existing/custom.tests.ps1
@@ -1,11 +1,3 @@
-######################################
-## Additional post-deployment tests ##
-######################################
-##
-## You can add any custom post-deployment validation tests you want here, or add them spread accross multiple test files in the test case folder.
-##
-###########################
-
 param (
     [Parameter(Mandatory = $false)]
     [hashtable] $TestInputData = @{}
@@ -13,16 +5,43 @@ param (
 
 Describe 'Validate deployment with provided Azure Log Analytics Workspace' {
 
-    Context 'Validate sucessful deployment' {
+    BeforeAll {
+        $resourceId = $TestInputData.DeploymentOutputs.resourceId.Value
+        $name = $TestInputData.DeploymentOutputs.name.Value
+        $location = $TestInputData.DeploymentOutputs.location.Value
+        $resourceGroupName = $TestInputData.DeploymentOutputs.resourceGroupName.Value
+
+        $virtualNetworkResourceId = $TestInputData.DeploymentOutputs.virtualNetworkResourceId.Value
+        $virtualNetworkName = $TestInputData.DeploymentOutputs.virtualNetworkName.Value
+        $virtualNetworkLocation = $TestInputData.DeploymentOutputs.virtualNetworkLocation.Value
+        $resourceGroupName = $TestInputData.DeploymentOutputs.resourceGroupName.Value
+
+        $logAnalyticsWorkspaceResourceId = $TestInputData.DeploymentOutputs.logAnalyticsWorkspaceResourceId.Value
+        $logAnalyticsWorkspaceName = $TestInputData.DeploymentOutputs.logAnalyticsWorkspaceName.Value
+        $logAnalyticsWorkspaceLocation = $TestInputData.DeploymentOutputs.logAnalyticsWorkspaceLocation.Value
+        $logAnalyticsWorkspaceResourceGroupName = $TestInputData.DeploymentOutputs.logAnalyticsWorkspaceResourceGroupName.Value
+
+        $keyVaultResourceId = $TestInputData.DeploymentOutputs.keyVaultResourceId.Value
+        $keyVaultName = $TestInputData.DeploymentOutputs.keyVaultName.Value
+        $keyVaultLocation = $TestInputData.DeploymentOutputs.keyVaultLocation.Value
+        $keyVaultResourceGroupName = $TestInputData.DeploymentOutputs.keyVaultResourceGroupName.Value
+    }
+
+    Context 'Monitoring - Azure Log Analytics Workspace Tests' {
+
+        BeforeAll {
+        }
 
         It 'Check Azure Log Analytics Workspace' {
 
-            $logAnalyticsWorkspaceResourceId = $TestInputData.DeploymentOutputs.logAnalyticsWorkspaceResourceId.Value
-            $logAnalyticsWorkspaceName = $TestInputData.DeploymentOutputs.logAnalyticsWorkspaceName.Value
-            $logAnalyticsWorkspaceLocation = $TestInputData.DeploymentOutputs.logAnalyticsWorkspaceLocation.Value
-            $logAnalyticsWorkspaceResourceGroupName = $TestInputData.DeploymentOutputs.logAnalyticsWorkspaceResourceGroupName.Value
+            $logAnalyticsWorkspaceResourceId | Should -Not -BeNullOrEmpty
+            $logAnalyticsWorkspaceName | Should -Not -BeNullOrEmpty
+            $logAnalyticsWorkspaceLocation | Should -Not -BeNullOrEmpty
+            $logAnalyticsWorkspaceResourceGroupName.Name | Should -Not -BeNullOrEmpty
 
-            Get-AzOperationalInsightsWorkspace -ResourceGroupName $logAnalyticsWorkspaceResourceGroupName -Name $logAnalyticsWorkspaceName
+            $log = Get-AzOperationalInsightsWorkspace -ResourceGroupName $logAnalyticsWorkspaceResourceGroupName -Name $logAnalyticsWorkspaceName -ErrorAction SilentlyContinue
+
+            $log.Sku | Should -Be 'PerGB2018'
         }
     }
 }
