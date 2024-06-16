e176dbbca96e1c0860a35c23befb55fe25ee774d
AlexanderSehr <asehr@hotmail.de>
2024-06-16 14:30:33 +0200

    Update to latest

--- avm/utilities/tests/Test-CI.ps1
+++ avm/utilities/tests/Test-CI.ps1
@@ -1,4 +1,33 @@
-﻿
+﻿<#
+.SYNOPSIS
+This script executes either all CI-focused Pester tests, or a specific test file and generates a markdown with the test results.
+
+.DESCRIPTION
+This script executes either all CI-focused Pester tests, or a specific test file and generates a markdown with the test results.
+It only considers test files in its current or sub-folders
+
+.PARAMETER RepoRootPath
+Optional. The root of the repository. Used to correctly resolve paths.
+
+.PARAMETER BranchName
+Optional. The branch to test for.
+
+.PARAMETER TestFileRegex
+Optional. The regex to use when searching for test files
+
+.PARAMETER SkipOutput
+Optional. Set if you don't want the script to generate the results markdown
+
+.EXAMPLE
+Test-CI
+
+Invoke all CI-focused Pester tests and generate a markdown file with the test results.
+
+.EXAMPLE
+Test-CI -TestFileRegex '.*Ordered.*' -SkipOutput
+
+Invoke all CI-focused Pester tests that match the regex '.*Ordered.*' skip the generation of the markdown file containing the test results.
+#>
 function Test-CI {
 
     [CmdletBinding()]
@@ -10,14 +39,22 @@ function Test-CI {
         [string] $BranchName = (git branch --show-current),
 
         [Parameter()]
-        [string] $TestFile = '.*'
+        [string] $TestFileRegex = '.*',
+
+        [Parameter()]
+        [switch] $SkipOutput
     )
 
     # Load used functions
     . (Join-Path $RepoRootPath 'avm' 'utilities' 'pipelines' 'staticValidation' 'compliance' 'Set-PesterGitHubOutput.ps1')
 
     $testFiles = (Get-ChildItem -Path (Join-Path $RepoRootPath 'avm' 'utilities' 'tests') -Recurse -File -Filter '*.tests.ps1').FullName | Where-Object {
-        $_ -match $TestFile
+        $_ -match $TestFileRegex
+    }
+
+    if (-not $testFiles) {
+        Write-Warning "Skipping test execution as no test files matching the regex [$TestFileRegex] were found."
+        return
     }
 
     # ------------------- #
@@ -40,25 +77,28 @@ function Test-CI {
 
     $testResults = Invoke-Pester -Configuration $pesterConfiguration
 
-    # ----------------------------------------- #
-    # Create formatted Pester Test Results File #
-    # ----------------------------------------- #
+    if (-not $SkipOutput) {
+        # ----------------------------------------- #
+        # Create formatted Pester Test Results File #
+        # ----------------------------------------- #
 
-    $functionInput = @{
-        RepoRootPath      = $RepoRootPath
-        PesterTestResults = $testResults
-        OutputFilePath    = Join-Path $RepoRootPath 'avm' 'utilities' 'tests' 'Pester-output.md'
-        GitHubRepository  = $RepoRootPath
-        BranchName        = $BranchName
+        $functionInput = @{
+            RepoRootPath      = $RepoRootPath
+            PesterTestResults = $testResults
+            OutputFilePath    = Join-Path $RepoRootPath 'avm' 'utilities' 'tests' 'Pester-output.md'
+            GitHubRepository  = $RepoRootPath
+            BranchName        = $BranchName
+        }
+
+        Write-Verbose 'Invoke Pester formatting function with' -Verbose
+        Write-Verbose ($functionInput | ConvertTo-Json -Depth 0 | Out-String) -Verbose
+
+        Set-PesterGitHubOutput @functionInput -Verbose
+
+        return $functionInput.OutputFilePath
     }
-
-    Write-Verbose 'Invoke Pester formatting function with' -Verbose
-    Write-Verbose ($functionInput | ConvertTo-Json -Depth 0 | Out-String) -Verbose
-
-    Set-PesterGitHubOutput @functionInput -Verbose
-
-    return $functionInput.OutputFilePath
 }
+
 
 
 
