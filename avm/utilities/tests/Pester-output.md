14da135d044c88270d33d72a3bf48f314dc39e16
AlexanderSehr <asehr@hotmail.de>
2024-06-16 10:13:36 +0200

    Finished first draft of test workflow

---
+++ avm/utilities/tests/Pester-output.md
@@ -0,0 +1,42 @@
+# Pester validation summary 
+
+| Total No. of Processed Tests| Passed Tests :white_check_mark: | Failed Tests :x: | Skipped Tests :paperclip: | Tests with warnings :warning: |
+| :-- | :-- | :-- | :-- | :-- |
+| 3 | 3 | 0 | 0 | 0 |
+
+
+<details>
+<summary>List of failed Tests</summary>
+
+No tests failed.
+
+</details>
+
+
+<details>
+<summary>List of passed Tests</summary>
+
+| Name | Source |
+| :-- | :-- |
+| Test sequence ordering / **Remove first sequence should be as expected** | <code>[Get-OrderedResourcesList.tests.ps1:10](https://github.com/C:\dev\ip\bicep-registry-modules\fork-AlexanderSehr/blob/users/alsehr/pesterCiTests/avm\utilities\tests\pipelines\Get-OrderedResourcesList.tests.ps1#L10)</code> |
+| Test sequence ordering / **Remove last sequence should be as expected** | <code>[Get-OrderedResourcesList.tests.ps1:77](https://github.com/C:\dev\ip\bicep-registry-modules\fork-AlexanderSehr/blob/users/alsehr/pesterCiTests/avm\utilities\tests\pipelines\Get-OrderedResourcesList.tests.ps1#L77)</code> |
+| Test sequence ordering / **Remove sequence should be as expected if both first & last priorities are provided** | <code>[Get-OrderedResourcesList.tests.ps1:146](https://github.com/C:\dev\ip\bicep-registry-modules\fork-AlexanderSehr/blob/users/alsehr/pesterCiTests/avm\utilities\tests\pipelines\Get-OrderedResourcesList.tests.ps1#L146)</code> |
+
+</details>
+
+
+<details>
+<summary>List of skipped Tests</summary>
+
+No tests were skipped.
+
+</details>
+
+
+<details>
+<summary>List of explicit warnings</summary>
+
+No tests with warnings.
+
+</details>
+
---
+++ avm/utilities/tests/Test-CI.ps1
@@ -0,0 +1,67 @@
+﻿
+function Test-CI {
+
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string] $RepoRootPath = (Get-Item $PSScriptRoot).Parent.Parent.Parent.FullName,
+
+        [Parameter()]
+        [string] $BranchName = (git branch --show-current),
+
+        [Parameter()]
+        [string] $TestFile = '.*',
+
+        [Parameter()]
+        [string] $TestCase # TODO: Add support
+    )
+
+    # Load used functions
+    . (Join-Path $RepoRootPath 'avm' 'utilities' 'pipelines' 'staticValidation' 'compliance' 'Set-PesterGitHubOutput.ps1')
+
+    $testFiles = (Get-ChildItem -Path (Join-Path $RepoRootPath 'avm' 'utilities' 'tests') -Recurse -File -Filter '*.tests.ps1').FullName | Where-Object {
+        $_ -match $TestFile
+    }
+
+    # ------------------- #
+    # Invoke Pester tests #
+    # ------------------- #
+    $pesterConfiguration = @{
+        Run    = @{
+            Container = New-PesterContainer -Path $testFiles -Data @{
+                RepoRootPath = $RepoRootPath
+            }
+            PassThru  = $true
+        }
+        Output = @{
+            Verbosity = 'Detailed'
+        }
+    }
+
+    Write-Verbose 'Invoke test with' -Verbose
+    Write-Verbose ($pesterConfiguration | ConvertTo-Json -Depth 4 | Out-String) -Verbose
+
+    $testResults = Invoke-Pester -Configuration $pesterConfiguration
+
+    # ----------------------------------------- #
+    # Create formatted Pester Test Results File #
+    # ----------------------------------------- #
+
+    $functionInput = @{
+        RepoRootPath      = $RepoRootPath
+        PesterTestResults = $testResults
+        OutputFilePath    = Join-Path $RepoRootPath 'avm' 'utilities' 'tests' 'Pester-output.md'
+        GitHubRepository  = $RepoRootPath
+        BranchName        = $BranchName
+    }
+
+    Write-Verbose 'Invoke Pester formatting function with' -Verbose
+    Write-Verbose ($functionInput | ConvertTo-Json -Depth 0 | Out-String) -Verbose
+
+    Set-PesterGitHubOutput @functionInput -Verbose
+
+    return $functionInput.OutputFilePath
+}
+
+
+
