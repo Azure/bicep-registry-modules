ccbec38ada13469391e5e0756412485043711ef0
AlexanderSehr <asehr@hotmail.de>
2024-07-09 09:45:42 +0200

    Update to latest

--- avm/utilities/pipelines/staticValidation/compliance/Set-PesterGitHubOutput.ps1
+++ avm/utilities/pipelines/staticValidation/compliance/Set-PesterGitHubOutput.ps1
@@ -12,35 +12,35 @@ Optional. The path to the root of the repository
 Mandatory. The Pester tests results to parse. Can be fetched by running Pester with the `-PassThru` parameter. For example:
 
 @{
-        Containers                        = '[+] C:/ResourceModules/utilities/pipelines/staticValidation/module.tests.ps1'
-        Result                                = 'Passed'
-        FailedCount                     = 0
-        FailedBlocksCount         = 0
+        Containers            = '[+] C:/ResourceModules/utilities/pipelines/staticValidation/module.tests.ps1'
+        Result                = 'Passed'
+        FailedCount           = 0
+        FailedBlocksCount     = 0
         FailedContainersCount = 0
-        PassedCount                     = 36
-        SkippedCount                    = 1
-        NotRunCount                     = 0
-        TotalCount                        = 37
-        Duration                            = '00:00:41.8816077'
-        Executed                            = true
-        ExecutedAt                        = '2023-04-01T12:27:19.5807422+02:00'
-        Version                             = '5.4.0'
-        PSVersion                         = '7.3.3'
-        PSBoundParameters         = 'System.Management.Automation.PSBoundParametersDictionary'
-        Plugins                             = null
-        PluginConfiguration     = null
-        PluginData                        = null
-        Configuration                 = 'PesterConfiguration'
-        DiscoveryDuration         = '00:00:16.1489218'
-        UserDuration                    = '00:00:22.4714890'
-        FrameworkDuration         = '00:00:03.2611969'
-        Failed                                = ''
-        FailedBlocks                    = ''
-        FailedContainers            = ''
-        Passed                                = '[+] [key-vault/vault/secret] Module should contain a [main.json/main.bicep] file. [+] ...'
-        NotRun                                = ''
-        Tests                                 = '[+] [key-vault/vault/secret] Module should contain a [main.json/main.bicep] file. [+] ...'
-        CodeCoverage                    = null
+        PassedCount           = 36
+        SkippedCount          = 1
+        NotRunCount           = 0
+        TotalCount            = 37
+        Duration              = '00:00:41.8816077'
+        Executed              = true
+        ExecutedAt            = '2023-04-01T12:27:19.5807422+02:00'
+        Version               = '5.4.0'
+        PSVersion             = '7.3.3'
+        PSBoundParameters     = 'System.Management.Automation.PSBoundParametersDictionary'
+        Plugins               = null
+        PluginConfiguration   = null
+        PluginData            = null
+        Configuration         = 'PesterConfiguration'
+        DiscoveryDuration     = '00:00:16.1489218'
+        UserDuration          = '00:00:22.4714890'
+        FrameworkDuration     = '00:00:03.2611969'
+        Failed                = ''
+        FailedBlocks          = ''
+        FailedContainers      = ''
+        Passed                = '[+] [key-vault/vault/secret] Module should contain a [main.json/main.bicep] file. [+] ...'
+        NotRun                = ''
+        Tests                 = '[+] [key-vault/vault/secret] Module should contain a [main.json/main.bicep] file. [+] ...'
+        CodeCoverage          = null
 }
 
 .PARAMETER OutputFilePath
@@ -121,7 +121,7 @@ function Set-PesterGitHubOutput {
     )
 
     ######################
-    ##     Failed Tests     ##
+    ##   Failed Tests   ##
     ######################
     Write-Verbose 'Adding failed tests'
     $fileContent += [System.Collections.ArrayList]@(
@@ -174,7 +174,7 @@ function Set-PesterGitHubOutput {
     )
 
     ######################
-    ##     Passed Tests     ##
+    ##   Passed Tests   ##
     ######################
     Write-Verbose 'Adding passed tests'
     $fileContent += [System.Collections.ArrayList]@(
@@ -225,7 +225,7 @@ function Set-PesterGitHubOutput {
     )
 
     #######################
-    ##     Skipped Tests     ##
+    ##   Skipped Tests   ##
     #######################
 
     Write-Verbose 'Adding skipped tests'
@@ -279,7 +279,7 @@ function Set-PesterGitHubOutput {
     )
 
     ##################
-    ##     Warnings     ##
+    ##   Warnings   ##
     ##################
 
     Write-Verbose 'Adding warnings'
