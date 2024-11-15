76b21b5cc6281fcae92bc1703162d75052083efa
Matthew Bratschun <matthew.bratschun@microsoft.com>
2024-11-15 14:21:21 -0700

    mgmt ips to static

--- avm/res/azure-stack-hci/cluster/tests/_template-assets/azureStackHCIHost/scripts/hciHostStage7.ps1
+++ avm/res/azure-stack-hci/cluster/tests/_template-assets/azureStackHCIHost/scripts/hciHostStage7.ps1
@@ -153,4 +153,16 @@ $vmNicLocalNamingOut = Invoke-Command -VMName (Get-VM).Name -Credential $adminCr
     }
 }
 
+# change dynamically assigned mgmt IP addresses to static IPs as required by validation
+Invoke-Command -VMName (Get-VM).Name -Credential $adminCred {
+    $ErrorActionPreference = 'Stop'
+
+    $dhcpIpConfig = Get-NetIPConfiguration -InterfaceAlias 'mgmt'
+    $prefixLength = Get-NetIPAddress -InterfaceAlias 'mgmt' | Select-Object -ExpandProperty PrefixLength
+
+    Set-NetIPInterface -InterfaceAlias 'mgmt' -Dhcp Disabled
+
+    New-NetIPAddress -InterfaceAlias 'mgmt' -IPAddress $dhcpIpConfig.IPAddress -DefaultGateway $dhcpIpConfig.DefaultGateway -AddressFamily IPv4 -PrefixLength $prefixLength
+}
+
 log "VM NIC local naming output: $vmNicLocalNamingOut"
