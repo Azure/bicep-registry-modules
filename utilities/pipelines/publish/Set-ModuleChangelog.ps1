d4cea7ed34e0d977b7ea877128866c20a4ae5ab4
René Hézser <rehezser@microsoft.com>
2024-12-20 17:47:12 +0100

    wip

---
+++ utilities/pipelines/publish/Set-ModuleChangelog.ps1
@@ -0,0 +1,27 @@
+﻿function Set-ModuleChangelog {
+
+    [CmdletBinding()]
+    param (
+        # avm/res/app/job/CHANGELOG.md avm/res/app/job/README.md
+        [Parameter(Mandatory)]
+        [array] $Files
+    )
+
+    $FileArray = $Files.split(' ')
+    Write-Host "Setting module changelog - FileArray: $FileArray"
+
+    # $ProcessedFiles = @()
+    # $FileArray | ForEach-Object {
+    #     $splitArray = $_ -split '/'
+    #     $modulePath = "$($splitArray[1])/$($splitArray[2])/$($splitArray[3])"
+    #     $module = $splitArray[4]
+    #     $ProcessedFiles += [PSCustomObject]@{ ModulePath = $modulePath; Module = $module }
+    # }
+
+    $FilteredFiles = $FileArray | Where-Object { $_ -contains 'CHANGELOG.md' }
+
+    $FilteredFiles | ForEach-Object {
+        Write-Host "Module: $_"
+    }
+
+}
