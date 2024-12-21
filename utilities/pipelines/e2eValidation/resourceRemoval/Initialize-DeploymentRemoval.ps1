0fe8359bcf5330cdc0e26f33926e7bd624a9c346
AlexanderSehr <asehr@hotmail.de>
2024-12-21 21:41:41 +0100

    Update to latest

--- utilities/pipelines/e2eValidation/resourceRemoval/Initialize-DeploymentRemoval.ps1
+++ utilities/pipelines/e2eValidation/resourceRemoval/Initialize-DeploymentRemoval.ps1
@@ -94,7 +94,8 @@ function Initialize-DeploymentRemoval {
             'Microsoft.Compute/virtualMachines',
             'Microsoft.ContainerInstance/containerGroups' # Must be removed before their MSI
             'Microsoft.ManagedIdentity/userAssignedIdentities',
-            'Microsoft.Databricks/workspaces'
+            'Microsoft.Databricks/workspaces',
+            'Microsoft.NetApp/netAppAccounts',
             'Microsoft.Resources/resourceGroups'
         )
 
