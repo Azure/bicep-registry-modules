36c9bd5cb3ab121266104456dfefffc75652fefd
AlexanderSehr <asehr@hotmail.de>
2025-01-22 17:53:38 +0100

    Small fix to module installation

--- utilities/pipelines/sharedScripts/Set-EnvironmentOnAgent.ps1
+++ utilities/pipelines/sharedScripts/Set-EnvironmentOnAgent.ps1
@@ -65,8 +65,10 @@ function Install-CustomModule {
                 # Get latest in case of multiple
                 $alreadyInstalled = ($alreadyInstalled | Sort-Object -Culture 'en-US' -Property 'Version' -Descending)[0]
             }
-            Write-Verbose ('Module [{0}] already installed with version [{1}]' -f $alreadyInstalled.Name, $alreadyInstalled.Version) -Verbose
-            continue
+            if ($alreadyInstalled) {
+                Write-Verbose ('Module [{0}] already installed with version [{1}]' -f $alreadyInstalled.Name, $alreadyInstalled.Version) -Verbose
+                continue
+            }
         }
 
         # Check if not to be excluded
