5c42d3564c366a679345d6fd9ced10bf3e855dd5
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2025-01-23 15:17:57 +0200

    fix: replace curl with wget for downloading Microsoft repository GPG keys in Set-EnvironmentOnAgent.ps1

--- utilities/pipelines/sharedScripts/Set-EnvironmentOnAgent.ps1
+++ utilities/pipelines/sharedScripts/Set-EnvironmentOnAgent.ps1
@@ -278,7 +278,7 @@ if ($InstallLatestPwshVersion) {
     # Install pre-requisite packages.
     sudo apt-get install -y wget apt-transport-https software-properties-common
     # Download the Microsoft repository GPG keys
-    curl -q "https://packages.microsoft.com/config/ubuntu/`$(lsb_release -rs)/packages-microsoft-prod.deb"
+    wget -q "https://packages.microsoft.com/config/ubuntu/`$(lsb_release -rs)/packages-microsoft-prod.deb"
     # Register the Microsoft repository GPG keys
     sudo dpkg -i packages-microsoft-prod.deb
     # Update the list of packages after we added packages.microsoft.com
