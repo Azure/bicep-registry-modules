7f736d78f5e74fc6fecc49d668d89fdc2e7f207a
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2025-01-26 11:58:36 +0200

    fix: enable Az.Accounts module import in resource removal script

--- utilities/pipelines/e2eValidation/resourceRemoval/helper/Invoke-ResourceRemoval.ps1
+++ utilities/pipelines/e2eValidation/resourceRemoval/helper/Invoke-ResourceRemoval.ps1
@@ -269,7 +269,7 @@ function Invoke-ResourceRemoval {
             if ($subscriptionState -eq 'Enabled') {
                 Write-Verbose ('[*] Disabling resource [{0}] of type [{1}]' -f $subscriptionName, $Type) -Verbose
                 if ($PSCmdlet.ShouldProcess("Subscription [$subscriptionName]", 'Remove')) {
-                    #Import-Module Az.Accounts -MinimumVersion 4.0.1
+                    Import-Module Az.Accounts -MinimumVersion 4.0.1
                     $null = Disable-AzSubscription -SubscriptionId $subscriptionId -Confirm:$false
                 }
             }
