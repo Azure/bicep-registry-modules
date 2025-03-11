4bdc1aebbc52b968c898f0c301255bea62f2fc6a
Alvaro Guadamillas <alguadam@microsoft.com>
2025-03-11 02:06:20 +0100

    refactor: Remove unused location configurations and deployment scripts

--- avm/ptn/sa/conversation-knowledge-mining/tests/local/deploy.ps1
+++
@@ -1,3 +0,0 @@
-##az deployment group create --resource-group 'avm-dev' --template-file './local.test.bicep'
-##Test-AzResourceGroupDeployment -ResourceGroupName 'avm-dev' -TemplateFile './local.test.bicep' -Verbose
-New-AzResourceGroupDeployment -ResourceGroupName 'avm-dev' -TemplateFile './local.test.bicep' -DeploymentName 'local-tests' -Verbose
