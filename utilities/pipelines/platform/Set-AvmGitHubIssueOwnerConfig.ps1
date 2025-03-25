bb3764bfac26e35d2ec7e884bc2dbadb68e2ddd9
Rainer Halanek <rahalan@microsoft.com>
2025-03-25 15:40:08 +0100

    add gh issue label logic

--- utilities/pipelines/platform/Set-AvmGitHubIssueOwnerConfig.ps1
+++ utilities/pipelines/platform/Set-AvmGitHubIssueOwnerConfig.ps1
@@ -37,7 +37,7 @@ function Set-AvmGitHubIssueOwnerConfig {
     . (Join-Path $RepoRoot 'utilities' 'pipelines' 'platform' 'helper' 'Get-AvmCsvData.ps1')
     . (Join-Path $RepoRoot 'utilities' 'pipelines' 'platform' 'helper' 'Add-GithubIssueToProject.ps1')
 
-    $issue = gh issue view $IssueUrl.Replace('api.', '').Replace('repos/', '') --json 'author,title,url,body,comments' --repo $Repo | ConvertFrom-Json -Depth 100
+    $issue = gh issue view $IssueUrl.Replace('api.', '').Replace('repos/', '') --json 'author,title,url,assignees,body,comments' --repo $Repo | ConvertFrom-Json -Depth 100
 
     if ($issue.title.StartsWith('[AVM Module Issue]')) {
         $moduleName = ($issue.body.Split("`n") -match 'avm/(?:res|ptn|utl)')[0].Trim().Replace(' ', '')
@@ -58,15 +58,33 @@ function Set-AvmGitHubIssueOwnerConfig {
 > [!IMPORTANT]
 > The module does not exist yet, we look into it. Please file a new module proposal under [AVM Module proposal](https://aka.ms/avm/moduleproposal).
 "@
+            if ($issue.comments.body -notcontains $reply) {
+                if ($PSCmdlet.ShouldProcess("reply comment to issue [$($issue.title)]")) {
+                    # write comment
+                    gh issue comment $issue.url --body $reply --repo $Repo
+                }
+            }
         }
         # orphaned module
         elseif ($module.ModuleStatus -eq 'Orphaned :eyes:') {
+            if ($issue.labels.name -notcontains 'Status: Module Orphaned :eyes:') {
+                if ($PSCmdlet.ShouldProcess("add orphaned label to issue [$($issue.title)]")) {
+                    gh issue edit $issue.url --add-label 'Status: Module Orphaned :eyes:' --repo $Repo
+                }
+            }
+
             $reply = @"
 **@$($issue.author.login), thanks for submitting this issue for the ``$moduleName`` module!**
 
 > [!IMPORTANT]
 > Please note, that this module is currently orphaned. The @Azure/avm-core-team-technical-bicep, will attempt to find an owner for it. In the meantime, the core team may assist with this issue. Thank you for your patience!
 "@
+            if ($issue.comments.body -notcontains $reply) {
+                if ($PSCmdlet.ShouldProcess("reply comment to issue [$($issue.title)]")) {
+                    # write comment
+                    gh issue comment $issue.url --body $reply --repo $Repo
+                }
+            }
         }
         # existing module
         else {
@@ -76,39 +94,48 @@ function Set-AvmGitHubIssueOwnerConfig {
 > [!IMPORTANT]
 > A member of the @Azure/$($module.ModuleOwnersGHTeam) or @Azure/$($module.ModuleContributorsGHTeam) team will review it soon!
 "@
+            if ($issue.comments.body -notcontains $reply) {
+                if ($PSCmdlet.ShouldProcess("reply comment to issue [$($issue.title)]")) {
+                    # write comment
+                    gh issue comment $issue.url --body $reply --repo $Repo
+                }
+            }
         }
 
         # add issue to project
         $ProjectNumber = 566 # AVM - Module Issues
         Add-GithubIssueToProject -Repo $Repo -ProjectNumber $ProjectNumber -IssueUrl $IssueUrl
 
-        if ($PSCmdlet.ShouldProcess("class label to issue [$($issue.title)]", 'Add')) {
-            gh issue edit $issue.url --add-label ($moduleIndex -eq 'Bicep-Resource' ? 'Class: Resource Module :package:' : 'Class: Pattern Module :package:') --repo $Repo
+        $moduleTypeLabel = $moduleIndex -eq 'Bicep-Resource' ? 'Class: Resource Module :package:' : 'Class: Pattern Module :package:'
+
+        if ($issue.labels.name -notcontains $moduleTypeLabel) {
+            if ($PSCmdlet.ShouldProcess("class label to issue [$($issue.title)]")) {
+                gh issue edit $issue.url --add-label $moduleTypeLabel --repo $Repo
+            }
         }
 
-        if ($PSCmdlet.ShouldProcess("reply comment to issue [$($issue.title)]", 'Add')) {
-            # write comment
-            gh issue comment $issue.url --body $reply --repo $Repo
-        }
-
-        if (($module.ModuleStatus -ne 'Orphaned :eyes:') -and (-not ([string]::IsNullOrEmpty($module.PrimaryModuleOwnerGHHandle)))) {
-            if ($PSCmdlet.ShouldProcess(("owner [{0}] to issue [$($issue.title)]" -f $module.PrimaryModuleOwnerGHHandle), 'Assign')) {
+        if (($module.ModuleStatus -ne 'Orphaned :eyes:') -and (-not ([string]::IsNullOrEmpty($module.PrimaryModuleOwnerGHHandle))) -and ($issue.assignees.login -notcontains $module.PrimaryModuleOwnerGHHandle)) {
+            if ($PSCmdlet.ShouldProcess(("owner [{0}] to issue [$($issue.title)]" -f $module.PrimaryModuleOwnerGHHandle))) {
                 # assign owner
                 $assign = gh issue edit $issue.url --add-assignee $module.PrimaryModuleOwnerGHHandle --repo $Repo
-            }
 
-            if ([String]::IsNullOrEmpty($assign)) {
-                if ($PSCmdlet.ShouldProcess("missing user comment to issue [$($issue.title)]", 'Add')) {
-                    $reply = @"
+                if ([String]::IsNullOrEmpty($assign)) {
+                    if ($PSCmdlet.ShouldProcess("missing user comment to issue [$($issue.title)]")) {
+                        $reply = @"
 > [!WARNING]
 > This issue couldn't be assigend due to an internal error. @$($module.PrimaryModuleOwnerGHHandle), please make sure this issue is assigned to you and please provide an initial response as soon as possible, in accordance with the [AVM Support statement](https://aka.ms/AVM/Support).
 "@
+                        gh issue comment $issue.url --body $reply --repo $Repo
+                    }
+                }
+            }
 
-                    gh issue comment $issue.url --body $reply --repo $Repo
+            if ($issue.labels.name -contains 'Status: Module Orphaned :eyes:') {
+                if ($PSCmdlet.ShouldProcess("remove orphaned label from issue [$($issue.title)]")) {
+                    gh issue edit $issue.url --remove-label 'Status: Module Orphaned :eyes:' --repo $Repo
                 }
             }
         }
+        Write-Verbose ('issue {0}{1} updated' -f $issue.title, $($WhatIfPreference ? ' would have been' : ''))
     }
-
-    Write-Verbose ('issue {0}{1} updated' -f $issue.title, $($WhatIfPreference ? ' would have been' : ''))
 }
