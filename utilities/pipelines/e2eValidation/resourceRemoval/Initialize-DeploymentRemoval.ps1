0054283c2f9d23f466fadfcd4542c9f9beed4473
Erika Gressi <erikag@microsoft.com>
2025-03-26 19:56:07 +0100

    cleanup removal

--- utilities/pipelines/e2eValidation/resourceRemoval/Initialize-DeploymentRemoval.ps1
+++ utilities/pipelines/e2eValidation/resourceRemoval/Initialize-DeploymentRemoval.ps1
@@ -104,8 +104,6 @@ function Initialize-DeploymentRemoval {
             'Microsoft.NetApp/netAppAccounts/capacityPools', # Must be deleted before netapp account because the Resource Provider does not allow deleting the account as long as it has nested resources
             'Microsoft.Network/virtualNetworkGateways', # Must be deleted before the Public IP that is associated with it
             'Microsoft.Network/loadBalancers', # Must be deleted before e.g. a GatewaySubnet that is associated with it
-            # 'Microsoft.DataProtection/backupVaults/backupInstances', # Must be deleted before backup vault because the Resource Provider does not allow deleting the vault as long as it has nested resources
-            # 'Microsoft.DataProtection/backupVaults/backupPolicies', # Must be deleted before backup vault because the Resource Provider does not allow deleting the vault as long as it has nested resources
             'Microsoft.DataProtection/backupVaults', # This resource has a custom removal logic and hence needs to be deleted before its resource group
             'Microsoft.Resources/resourceGroups'
         )
