beb71cf61a62e15795823c38512ff9d0c3df5226
AlexanderSehr <asehr@hotmail.de>
2025-04-05 16:23:01 +0200

    Pushing wip

--- utilities/pipelines/platform/Set-AvmGithubIssueForWorkflow.ps1
+++ utilities/pipelines/platform/Set-AvmGithubIssueForWorkflow.ps1
@@ -5,34 +5,40 @@ Check for failing pipelines and create issues for those, that are failing.
 .DESCRIPTION
 If a pipeline fails, a new issue will be created, with a link to the failed pipeline. If the issue is already existing, a comment will be added, if a new run failed (with the link for the new failed run). If a pipeline run succeeds and an issue is open for the failed run, it will be closed (and a link to the successful run is added to the issue).
 
-.PARAMETER Repo
-Mandatory. The name of the respository to scan. Needs to have the structure "<owner>/<repositioryName>", like 'Azure/bicep-registry-modules/'
+.PARAMETER RepositoryOwner
+Optional. The GitHub organization to run the workfows in.
+
+.PARAMETER RepositoryName
+Optional. The GitHub repository to run the workfows in.
 
 .PARAMETER RepoRoot
 Optional. Path to the root of the repository.
 
-.PARAMETER LimitNumberOfRuns
-Optional. Number of recent runs to scan for failed runs. Default is 100.
-
-.PARAMETER LimitInDays
-Optional. Only runs in the past selected days will be analyzed. Default is 2.
+.PARAMETER PersonalAccessToken
+Optional. The PAT to use to interact with either GitHub. If not provided, the script will use the GitHub CLI to authenticate.
 
 .PARAMETER IgnoreWorkflows
 Optional. List of workflow names that should be ignored (even if they fail, no ticket will be created). Default is an empty array.
 
 .EXAMPLE
-Set-AvmGithubIssueForWorkflow -Repo 'owner/repo01' -LimitNumberOfRuns 100 -LimitInDays 2 -IgnoreWorkflows @('Pipeline 01')
+Set-AvmGitHubIssueForWorkflow -Repo 'owner/repo01' -IgnoreWorkflows @('Pipeline 01')
 
 Check the last 100 workflow runs in the repository 'owner/repo01' that happened in the last 2 days. If the workflow name is 'Pipeline 01', then ignore the workflow run.
 
 .NOTES
 Will be triggered by the workflow platform.manage-workflow-issue.yml
 #>
-function Set-AvmGithubIssueForWorkflow {
+function Set-AvmGitHubIssueForWorkflow {
     [CmdletBinding(SupportsShouldProcess)]
     param (
-        [Parameter(Mandatory = $true)]
-        [string] $Repo,
+        [Parameter(Mandatory = $false)]
+        [string] $PersonalAccessToken,
+
+        [Parameter(Mandatory = $false)]
+        [string] $RepositoryOwner = 'Azure',
+
+        [Parameter(Mandatory = $false)]
+        [string] $RepositoryName = 'bicep-registry-modules',
 
         [Parameter(Mandatory = $false)]
         [string] $RepoRoot = (Get-Item -Path $PSScriptRoot).parent.parent.parent.FullName,
@@ -50,10 +56,172 @@ function Set-AvmGithubIssueForWorkflow {
     # Loading helper functions
     . (Join-Path $RepoRoot 'utilities' 'pipelines' 'platform' 'helper' 'Get-AvmCsvData.ps1')
     . (Join-Path $RepoRoot 'utilities' 'pipelines' 'platform' 'helper' 'Add-GithubIssueToProject.ps1')
+    . (Join-Path $RepoRoot 'utilities' 'pipelines' 'platform' 'helper' 'Get-GitHubModuleWorkflowList.ps1')
+    . (Join-Path $RepoRoot 'utilities' 'pipelines' 'platform' 'helper' 'Get-GitHubModuleWorkflowLatestRun.ps1')
+    . (Join-Path $RepoRoot 'utilities' 'pipelines' 'platform' 'helper' 'Get-GitHubIssueList.ps1')
 
+
+    ##################################
+    #   Loading module information   #
+    ##################################
     $knownPatterns = Get-AvmCsvData -ModuleIndex 'Bicep-Pattern'
     $knownResources = Get-AvmCsvData -ModuleIndex 'Bicep-Resource'
     $knownUtilities = Get-AvmCsvData -ModuleIndex 'Bicep-Utility'
+
+    #######################
+    #    Get all issues   #
+    #######################
+    $baseInputObject = @{
+        RepositoryOwner = $RepositoryOwner
+        RepositoryName  = $RepositoryName
+    }
+    if ($PersonalAccessToken) {
+        $baseInputObject['PersonalAccessToken'] = @{
+            PersonalAccessToken = $PersonalAccessToken
+        }
+    }
+
+    $issues = Get-GitHubIssueList @baseInputObject | Where-Object {
+        $_.title -match '\[Failed pipeline\] .+'
+    }
+
+    ##########################
+    #    Get all workflows   #
+    ##########################
+
+    Write-Verbose 'Fetching current GitHub workflows' -Verbose
+    $workflows = Get-GitHubModuleWorkflowList @baseInputObject -Filter $PipelineFilter
+    Write-Verbose ('Fetched [{0}] workflows' -f $workflows.Count) -Verbose
+
+    ############################################
+    #   Fetching latest run of each workflow   #
+    ############################################
+    $workflowRunsToProcess = [System.Collections.ArrayList]@()
+    $totalCount = $workflows.Count
+    $currentCount = 1
+    # foreach ($workflow in $workflows) {
+
+    #     $percentageComplete = [math]::Round(($currentCount / $totalCount) * 100)
+    #     Write-Progress -Activity ('Fetching workflow [{0}]' -f $workflow.name) -Status "$percentageComplete% complete" -PercentComplete $percentageComplete
+    #     # Get relevant run
+    #     $latestWorkflowRun = Get-GitHubModuleWorkflowLatestRun @baseInputObject -WorkflowId $workflow.id
+
+    #     if ($latestWorkflowRun.status -eq 'completed') {
+    #         $workflowRunsToProcess += $latestWorkflowRun
+    #     }
+
+    #     $currentCount++
+    # }
+    # TODO: Replace with above
+    $workflowRunsToProcess = Get-Content -Path 'C:\Users\alsehr\OneDrive - Microsoft\Docs\Code\temp\workflowsDump.json' | ConvertFrom-Json
+
+    ############################
+    #   Processing workflows   #
+    ############################
+    $issuesCreated = 0
+    $issuesCommented = 0
+    $issuesClosed = 0
+    foreach ($workflowRun in $workflowRunsToProcess) {
+        if ($workflowRun.conclusion -eq 'failure') {
+            $issueName = "[Failed pipeline] $($workflowRun.name)"
+            $failedrun = "Failed run: $($workflowRun.url)"
+            $moduleName = $workflowRun.name.Replace('.', '/')
+
+            if ($issues.title -notcontains $issueName) {
+                if ($PSCmdlet.ShouldProcess("Issue [$issueName]", 'Create')) {
+                    $issueUrl = gh issue create --title "$issueName" --body "$failedrun" --label 'Type: AVM :a: :v: :m:,Type: Bug :bug:' --repo "$RepositoryOwner/$RepositoryName"
+                }
+
+                $ProjectNumber = 538 # AVM - Issue Triage
+                $comment = @"
+> [!IMPORTANT]
+> This module is currently orphaned (has no owner), therefore expect a higher response time.
+> @Azure/avm-core-team-technical-bicep, the workflow for the ``$moduleName`` module has failed. Please investigate the failed workflow run.
+"@
+
+                if ($workflowRun.name -match 'avm.(?:res|ptn|utl)') {
+                    switch ($matches[0]) {
+                        'avm.ptn' { $module = $knownPatterns | Where-Object { $_.ModuleName -eq $moduleName }; break }
+                        'avm.res' { $module = $knownResources | Where-Object { $_.ModuleName -eq $moduleName }; break }
+                        'avm.utl' { $module = $knownUtilities | Where-Object { $_.ModuleName -eq $moduleName }; break }
+                        Default {
+                            throw 'Impossible regex condition.'
+                        }
+                    }
+
+                    if (($module.ModuleStatus -ne 'Orphaned :eyes:') -and (-not ([string]::IsNullOrEmpty($module.PrimaryModuleOwnerGHHandle)))) {
+                        $ProjectNumber = 566 # AVM - Module Issues
+                        $comment = @"
+> [!IMPORTANT]
+> @Azure/$($module.ModuleOwnersGHTeam), the workflow for the ``$moduleName`` module has failed. Please investigate the failed workflow run. If you are not able to do so, please inform the AVM core team to take over.
+"@
+                        # assign owner
+                        if ($PSCmdlet.ShouldProcess(('Owner [{0}] to issue [{1}]' -f $module.PrimaryModuleOwnerGHHandle, $issueName), 'Assign')) {
+                            $assign = gh issue edit $issueUrl --add-assignee $module.PrimaryModuleOwnerGHHandle --repo "$RepositoryOwner/$RepositoryName"
+                        }
+                        if ([String]::IsNullOrEmpty($assign)) {
+                            $comment = @"
+> [!WARNING]
+> This issue couldn't be assigend due to an internal error. @$($module.PrimaryModuleOwnerGHHandle), please make sure this issue is assigned to you and please provide an initial response as soon as possible, in accordance with the [AVM Support statement](https://aka.ms/AVM/Support).
+"@
+                            if ($PSCmdlet.ShouldProcess("missing user comment to issue [$($issueName)]", 'Add')) {
+                                gh issue comment $issueUrl --body $comment --repo "$RepositoryOwner/$RepositoryName"
+                            }
+                        }
+                    }
+                }
+
+                if ($PSCmdlet.ShouldProcess("Issue [$issueName] to project [AVM - Issue Triage]", 'Add')) {
+                    Add-GithubIssueToProject -Repo "$RepositoryOwner/$RepositoryName" -ProjectNumber $ProjectNumber -IssueUrl $issueUrl
+                }
+
+                if ($PSCmdlet.ShouldProcess("Comment to issue [$issueName]", 'Add')) {
+                    gh issue comment $issueUrl --body $comment --repo "$RepositoryOwner/$RepositoryName"
+                }
+
+                $issuesCreated++
+            } else {
+                $issue = ($issues | Where-Object { $_.title -eq $issueName })[0]
+
+                if (-not $issue.body.Contains($failedrun)) {
+                    if ($issue.comments.length -eq 0) {
+                        if ($PSCmdlet.ShouldProcess("Issue [$issueName]", 'Add comment')) {
+                            gh issue comment $issue.url --body $failedrun --repo "$RepositoryOwner/$RepositoryName"
+                        }
+
+                        $issuesCommented++
+                    } else {
+                        if (-not $issue.comments.body.Contains($failedrun)) {
+                            if ($PSCmdlet.ShouldProcess("Issue [$issueName]", 'Close')) {
+                                gh issue comment $issue.url --body $failedrun --repo "$RepositoryOwner/$RepositoryName"
+                            }
+
+                            $issuesCommented++
+                        }
+                    }
+                }
+            }
+        } else {
+            # Fetch and close all issues that match the issue name and match the successful run
+            $issuesToClose = $issues | Where-Object {
+                $_.title -eq ('[Failed pipeline] {0}' -f $workflowRun.name)
+            }
+
+            foreach ($issue in $issuesToClose) {
+                $comment = 'Successful run: {0}' -f $workflowRun.url
+                if ($PSCmdlet.ShouldProcess(('Issue [{0}] with URL [{1}]' -f $issue.title, $issue.url), 'Close')) {
+                    gh issue close $issue.url --comment $comment --repo "$RepositoryOwner/$RepositoryName"
+                }
+                $issuesClosed++
+            }
+        }
+    }
+
+    Write-Verbose ('[{0}] issue(s){1} created' -f $issuesCreated, $($WhatIfPreference ? ' would have been' : ''))
+    Write-Verbose ('[{0}] issue(s){1} commented' -f $issuesCommented, $($WhatIfPreference ? ' would have been' : ''))
+    Write-Verbose ('[{0}] issue(s){1} closed' -f $issuesClosed, $($WhatIfPreference ? ' would have been' : ''))
+
+    return
 
     $issues = gh issue list --state open --limit 500 --label 'Type: AVM :a: :v: :m:,Type: Bug :bug:' --json 'title,url,body,comments,labels' --repo $Repo | ConvertFrom-Json -Depth 100
     $runs = gh run list --json 'url,workflowName,headBranch,startedAt' --limit $LimitNumberOfRuns --repo $Repo | ConvertFrom-Json -Depth 100
@@ -90,9 +258,9 @@ function Set-AvmGithubIssueForWorkflow {
 
     foreach ($workflowRun in $workflowRuns.values) {
         if ($workflowRun.conclusion -eq 'failure') {
-            $issueName = "[Failed pipeline] $($workflowRun.workflowName)"
+            $issueName = "[Failed pipeline] $($workflowRun.name)"
             $failedrun = "Failed run: $($workflowRun.url)"
-            $moduleName = $workflowRun.workflowName.Replace('.', '/')
+            $moduleName = $workflowRun.name.Replace('.', '/')
 
             if ($issues.title -notcontains $issueName) {
                 if ($PSCmdlet.ShouldProcess("Issue [$issueName]", 'Create')) {
@@ -104,7 +272,7 @@ function Set-AvmGithubIssueForWorkflow {
 > @Azure/avm-core-team-technical-bicep, the workflow for the ``$moduleName`` module has failed. Please investigate the failed workflow run.
 "@
 
-                    if ($workflowRun.workflowName -match 'avm.(?:res|ptn|utl)') {
+                    if ($workflowRun.name -match 'avm.(?:res|ptn|utl)') {
                         switch ($matches[0]) {
                             'avm.ptn' { $module = $knownPatterns | Where-Object { $_.ModuleName -eq $moduleName }; break }
                             'avm.res' { $module = $knownResources | Where-Object { $_.ModuleName -eq $moduleName }; break }
@@ -165,7 +333,7 @@ function Set-AvmGithubIssueForWorkflow {
                 }
             }
         } else {
-            $issueName = "[Failed pipeline] $($workflowRun.workflowName)"
+            $issueName = "[Failed pipeline] $($workflowRun.name)"
 
             if ($issues.title -contains $issueName) {
                 $issue = ($issues | Where-Object { $_.title -eq $issueName })[0]
---
+++ utilities/pipelines/platform/helper/Get-GitHubIssueList.ps1
@@ -0,0 +1,72 @@
+﻿function Get-GitHubIssueList {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [string] $PersonalAccessToken,
+
+        [Parameter(Mandatory = $true)]
+        [string] $RepositoryOwner,
+
+        [Parameter(Mandatory = $true)]
+        [string] $RepositoryName,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('open', 'closed', 'all')]
+        [string] $State = 'open',
+
+        [Parameter(Mandatory = $false)]
+        [string] $Assignee = '*',
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('created', 'updated', 'comments')]
+        [string] $SortBy = 'created',
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('asc', 'desc')]
+        [string] $SortDirection = 'desc'
+    )
+
+    $allIssues = @()
+
+    $page = 1
+    do {
+        $queryParameters = @(
+            "sort=$SortBy",
+            "direction=$SortDirection",
+            "assignee=$Assignee",
+            "state=$State",
+            'per_page=100',
+            "page=$page"
+        ) -join '&'
+        $queryUrl = "/repos/$RepositoryOwner/$RepositoryName/issues?$queryParameters"
+        if ($PersonalAccessToken) {
+            # Using PAT
+            $requestInputObject = @{
+                Method  = 'GET'
+                Uri     = "https://api.github.com$queryUrl"
+                Headers = @{
+                    Authorization = "Bearer $PersonalAccessToken"
+                }
+            }
+            $response = Invoke-RestMethod @requestInputObject
+        } else {
+            # Using GH API instead of 'gh workflow list' to get all results instead of just the first few
+            $requestInputObject = @(
+                '-H', 'Accept: application/vnd.github+json',
+                '-H', 'X-GitHub-Api-Version: 2022-11-28',
+                $queryUrl
+            )
+            $response = (gh api @requestInputObject | ConvertFrom-Json -AsHashtable)
+        }
+
+        $allIssues += $response | Where-Object {
+            # In the API, PRs are considered issues too. See 'Note' at https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#get-an-issue
+            $_.Keys -notcontains 'pull_request'
+        }
+
+        $page++
+    } while ($response)
+
+    return $allIssues
+}
