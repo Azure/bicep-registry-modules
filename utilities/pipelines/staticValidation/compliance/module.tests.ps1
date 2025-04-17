e8f41d5a132e12c433cce7fa215865a5c862f192
AlexanderSehr <asehr@hotmail.de>
2025-04-17 19:54:51 +0200

    Update to latest

--- utilities/pipelines/staticValidation/compliance/module.tests.ps1
+++ utilities/pipelines/staticValidation/compliance/module.tests.ps1
@@ -169,7 +169,7 @@ Describe 'File/folder tests' -Tag 'Modules' {
             }
         }
 
-        It '[<moduleFolderName>] Module should contain a [` ORPHANED.md `] file only if orphaned.' -TestCases ($moduleFolderTestCases | Where-Object { $_.isTopLevelModule }) {
+        It '[<moduleFolderName>] Module should contain a [` ORPHANED.md `] file only if orphaned.' -TestCases ($moduleFolderTestCases | Where-Object { $_.isTopLevelModule -and $_.versionFileExists }) {
 
             param(
                 [string] $moduleFolderPath,
