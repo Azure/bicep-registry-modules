953e6464ca96820cd30cbcadecb03f6e29d0e1bc
AlexanderSehr <asehr@hotmail.de>
2025-04-24 15:20:09 +0200

    Accounting for upcoming breaking change

--- utilities/pipelines/platform/Publish-ModuleFromTagToPBR.ps1
+++ utilities/pipelines/platform/Publish-ModuleFromTagToPBR.ps1
@@ -80,7 +80,7 @@ function Publish-ModuleFromTagToPBR {
     $publishInput = @(
         $moduleBicepFilePath
         '--target', ('br:{0}/public/bicep/{1}:{2}' -f $plainPublicRegistryServer, $moduleRelativeFolderPath, $targetVersion)
-        '--documentationUri', $documentationUri
+        '--documentation-uri', $documentationUri
         '--with-source'
         '--force'
     )
