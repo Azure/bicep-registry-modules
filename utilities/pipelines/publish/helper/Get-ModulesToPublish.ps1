84125edf7f057f6ff23255971ae75863e93e4662
AlexanderSehr <asehr@hotmail.de>
2025-08-18 10:24:58 +0200

    Added escape

--- utilities/pipelines/publish/helper/Get-ModulesToPublish.ps1
+++ utilities/pipelines/publish/helper/Get-ModulesToPublish.ps1
@@ -26,11 +26,17 @@ function Get-ModifiedFileList {
         $currentCommit = git rev-parse 'main' # Get the current main's commit
         $previousCommit = git rev-parse 'upstream/main^' # Get the previous main's commit in upstream
 
+        $retryCount = 0
         while ($currentCommit -eq $previousCommit) {
             Write-Warning 'Current and previous commits are the same. Trying again'
             git fetch 'upstream' 'main' -q # Fetch the latest changes from upstream main
             Start-Sleep 5 # Wait for git to finish fetching
             $previousCommit = git rev-parse 'upstream/main^' # Get the previous main's commit in upstream
+
+            if ($retryCount -ge 5) {
+                throw 'Failed to get a different previous commit after 5 retries. Exiting.'
+            }
+            $retryCount++
         }
 
         Write-Verbose ('Currently in upstream [main]. Fetching changes of current commit [{0}] against [main^-1] [{1}].' -f $currentCommit.Substring(0, 7), $previousCommit.Substring(0, 7)) -Verbose
@@ -39,11 +45,17 @@ function Get-ModifiedFileList {
         $currentCommit = git rev-parse --short=8 'HEAD' # Get the current commit
         $currentUpstreamCommit = git rev-parse 'upstream/main' # Get the previous main's commit in upstream
 
+        $retryCount = 0
         while ($currentCommit -eq $currentUpstreamCommit) {
             Write-Warning 'Current and commit and upstream main are the same. Trying again'
             git fetch 'upstream' 'main' -q # Fetch the latest changes from upstream main
             Start-Sleep 5 # Wait for git to finish fetching
             $currentUpstreamCommit = git rev-parse 'upstream/main' # Get the previous main's commit in upstream
+
+            if ($retryCount -ge 5) {
+                throw 'Failed to get a different previous commit after 5 retries. Exiting.'
+            }
+            $retryCount++
         }
         Write-Verbose ('{0} Fetching changes of current commit [{1}] against upstream [main] [{2}]' -f ($inUpstream ? "Currently in upstream [$currentBranch]." : 'Currently in a fork.'), $currentCommit.Substring(0, 7), $currentUpstreamCommit.Substring(0, 7)) -Verbose
         $diff = git diff --name-only --diff-filter=AM $currentUpstreamCommit
