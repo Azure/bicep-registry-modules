a4c85677dbba760b36c3c52049720ef7195f1f35
AlexanderSehr <asehr@hotmail.de>
2025-08-18 18:47:15 +0200

    Fixed small typo

--- utilities/pipelines/publish/helper/Get-ModulesToPublish.ps1
+++ utilities/pipelines/publish/helper/Get-ModulesToPublish.ps1
@@ -49,7 +49,7 @@ function Get-ModifiedFileList {
         Write-Verbose ('Fetching changes of current commit [{0}] against [main^] [{1}].' -f $currentCommit, $previousCommit) -Verbose
         $diff = git diff --name-only --diff-filter=AM $previousCommit
     } else {
-        Write-Verbose ('{0} branch [$currentBranch]' -f ($inUpstream ? 'Currently in the upstream' : 'Currently in the fork')) -Verbose
+        Write-Verbose ("{0} branch [$currentBranch]" -f ($inUpstream ? 'Currently in the upstream' : 'Currently in the fork')) -Verbose
         $currentCommit = git rev-parse --short=7 'HEAD' # Get the current commit
         $currentUpstreamCommit = git rev-parse --short=7 'upstream/main' # Get the previous main's commit in upstream
 
