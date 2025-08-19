b259e26d9fe3214f4109fda74a41b6759f994367
AlexanderSehr <asehr@hotmail.de>
2025-08-19 14:27:46 +0200

    Update to latest

--- utilities/pipelines/publish/helper/Get-ModulesToPublish.ps1
+++ utilities/pipelines/publish/helper/Get-ModulesToPublish.ps1
@@ -21,7 +21,8 @@ function Get-ModifiedFileList {
     $inUpstream = (git remote get-url origin) -match '\/Azure\/' # If in upstream the value would be [https://github.com/Azure/bicep-registry-modules.git]
 
     # Note: Fetches only the name of the modified files
-    if ($inUpstream -and $currentBranch -eq 'main') {
+    # if ($inUpstream -and $currentBranch -eq 'main') {
+    if ($true) {
         Write-Verbose 'Currently in the upstream branch [main].' -Verbose
 
         # Get the current and previous commit
