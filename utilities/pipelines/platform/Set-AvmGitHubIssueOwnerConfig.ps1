3f47da3fadeca8c3f85130a3a21a4ae063956d5f
AlexanderSehr <asehr@hotmail.de>
2025-09-07 17:10:15 +0200

    Update to latest

--- utilities/pipelines/platform/Set-AvmGitHubIssueOwnerConfig.ps1
+++ utilities/pipelines/platform/Set-AvmGitHubIssueOwnerConfig.ps1
@@ -129,7 +129,7 @@ function Set-AvmGitHubIssueOwnerConfig {
         if (-not $issue.title.StartsWith('[AVM Module Issue]')) {
             # Not a module issue. Skipping
             if ([String]::IsNullOrEmpty($issueCategory)) {
-                Write-Verbose ('    📎  Issue [{0}] {1}: Not a module issue and unknown category. Is skipped and should be reviewed & updated by maintainers.' -f $issue.number, $shortTitle) -Verbose
+                Write-Verbose ('    ⚠️  Issue [{0}] {1}: Not a module issue and unknown category. Is skipped and should be reviewed & updated by maintainers.' -f $issue.number, $shortTitle) -Verbose
             } else {
                 Write-Verbose ('    ℹ️  Issue [{0}] {1}: Not a module issue but [{2}]. Skipping' -f $issue.number, $shortTitle, $issueCategory) -Verbose
             }
