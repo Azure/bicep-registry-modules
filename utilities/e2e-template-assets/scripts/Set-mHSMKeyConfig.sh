96ac23b65382a366b8a3e165913a82a902ff0186
Erika Gressi <erikag@microsoft.com>
2025-11-27 21:15:50 +0100

    default role

--- utilities/e2e-template-assets/scripts/Set-mHSMKeyConfig.sh
+++ utilities/e2e-template-assets/scripts/Set-mHSMKeyConfig.sh
@@ -1,13 +1,14 @@
-${4:=Managed HSM Crypto Service Encryption User}
-echo "Checking role assignment for HSM: $1, Key: $2, Principal: $3, Role: $4"
+# Default role to assign to HSM key
+role="${4:-Managed HSM Crypto Service Encryption User}"
+echo "Checking role assignment for HSM: $1, Key: $2, Principal: $3, Role: $role"
 # Allow key reference via identity
-result=$(az keyvault role assignment list --hsm-name "$1" --scope "/keys/$2" --query "[?principalId == \`$3\` && roleName == \`$4\`]")
+result=$(az keyvault role assignment list --hsm-name "$1" --scope "/keys/$2" --query "[?principalId == \`$3\` && roleName == \`$role\`]")
 
 if [[ "$result" != "[]" ]]; then
   echo "Role assignment already exists."
 else
   echo "Role assignment not yet existing. Creating."
-  az keyvault role assignment create --hsm-name "$1" --role "$4" --scope "/keys/$2" --assignee $3
+  az keyvault role assignment create --hsm-name "$1" --role "$role" --scope "/keys/$2" --assignee $3
 fi
 
 # Allow usage via ARM
