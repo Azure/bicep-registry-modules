e1dce0ad266e6a78cc9c43484cde553c0e62a6a8
Alexander Sehr <alexander.sehr@microsoft.com>
2026-03-06 20:04:05 +0100

    Update to latest

--- utilities/pipelines/staticValidation/compliance/module.tests.ps1
+++ utilities/pipelines/staticValidation/compliance/module.tests.ps1
@@ -45,7 +45,7 @@ BeforeDiscovery {
     }
 
     # Building paths
-    $compiledTemplatesMap = Build-ViaRPC -BicepFilePath $pathsToBuild -PassThru -Verbose
+    $compiledTemplatesMap = Build-ViaRPC -BicepFilePath $pathsToBuild -PassThru -Verbose # TODO: Remove verbose
     $builtTestFileMap = @{}
     foreach ($path in $pathsToBuild) {
         $builtTestFileMap[$path] = $compiledTemplatesMap[$path] | ConvertFrom-Json -AsHashtable
