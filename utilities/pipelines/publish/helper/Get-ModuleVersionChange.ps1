ef5237a2a14bcc3b0e2909b1331ce708ba5d614a
Alexander Sehr <alexander.sehr@microsoft.com>
2026-08-07 18:27:03 +0200

    Update to latest

--- utilities/pipelines/publish/helper/Get-ModuleVersionChange.ps1
+++ utilities/pipelines/publish/helper/Get-ModuleVersionChange.ps1
@@ -1,4 +1,4 @@
-<#
+﻿<#
 .SYNOPSIS
 Get current and previous major and minor version, if different.
 
@@ -41,7 +41,7 @@ function Get-ModuleVersionChange {
     )
 
     . (Join-Path $RepoRoot 'utilities' 'pipelines' 'sharedScripts' 'Get-GitDiff.ps1')
-    $diff = Get-GitDiff -PathFilter $VersionFilePath -Verbose | Out-String
+    $diff = Get-GitDiff -PathFilter $VersionFilePath -Verbose -IncludeNonCommitted | Out-String
 
     if ($diff -match '\-\s*"version":\s*"([0-9]{1})\.([0-9]{1})".*') {
         $oldVersion = (New-Object System.Version($matches[1], $matches[2]))
